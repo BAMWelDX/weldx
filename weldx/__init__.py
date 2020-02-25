94209022188f5957897ef9e2fc8dd2b9fc4cc7aa
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-02-25 14:10:29 +0100

    add versioneer and formating

--- weldx/__init__.py
+++ weldx/__init__.py
@@ -1,8 +1,8 @@
 import warnings
 
-# setup unit registry
-from . import constants
-from .constants import WELDX_UNIT_REGISTRY as ureg
+__all__ = []
+
+# from .constants import WELDX_UNIT_REGISTRY as ureg
 from .constants import WELDX_QUANTITY as Q_
 
 with warnings.catch_warnings():
@@ -11,3 +11,8 @@ with warnings.catch_warnings():
 
 # asdf extensions and tags
 import weldx.asdf
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
