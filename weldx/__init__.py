6c109db26beea78988ae0608e420fa3c6a96a7fb
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-03-20 13:24:29 +0100

    add imports to main __init__.py

--- weldx/__init__.py
+++ weldx/__init__.py
@@ -1,6 +1,14 @@
 import warnings
 
-__all__ = []
+__all__ = ["geometry", "transformations", "utility", "asdf"]
+
+# geometry packages
+from weldx import geometry
+from weldx import transformations
+from weldx import utility
+
+# asdf extensions and tags
+from weldx import asdf
 
 # from .constants import WELDX_UNIT_REGISTRY as ureg
 from .constants import WELDX_QUANTITY as Q_
@@ -9,8 +17,6 @@ with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     Q_([])
 
-# asdf extensions and tags
-import weldx.asdf
 
 from ._version import get_versions
 
