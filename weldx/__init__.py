10030149db6562754b419aee281c1c7db502796b
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-03-27 15:13:53 +0100

    flake8 fixes

--- weldx/__init__.py
+++ weldx/__init__.py
@@ -5,9 +5,6 @@ from ._version import get_versions
 
 __all__ = ["geometry", "transformations", "utility", "asdf"]
 
-__version__ = get_versions()["version"]
-del get_versions
-
 # geometry packages
 import weldx.geometry
 import weldx.transformations
@@ -22,3 +19,6 @@ from .constants import WELDX_QUANTITY as Q_
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     Q_([])
+
+__version__ = get_versions()["version"]
+del get_versions
