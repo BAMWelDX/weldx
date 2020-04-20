7c5809f96389ef0f8dce53718ba3e3fa7632a9bc
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-04-20 13:06:45 +0200

    fix import order

--- weldx/__init__.py
+++ weldx/__init__.py
@@ -1,5 +1,9 @@
 import warnings
 
+__all__ = ["geometry", "transformations", "utility", "asdf", "Q_"]
+
+from weldx.constants import WELDX_QUANTITY as Q_  # isort:skip
+
 # asdf extensions and tags
 import weldx.asdf
 
@@ -10,10 +14,6 @@ import weldx.utility
 
 # versioneer
 from ._version import get_versions
-from .constants import WELDX_QUANTITY as Q_
-
-__all__ = ["geometry", "transformations", "utility", "asdf", "Q_"]
-
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
