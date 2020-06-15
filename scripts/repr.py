d4678f0b5f51c852d442afc743f60391a29bbe26
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-06-15 13:33:44 +0200

    add more examples for trace/geometry/csm

--- scripts/repr.py
+++ scripts/repr.py
@@ -1,5 +1,7 @@
 import weldx.geometry as geo
 import weldx.transformations as tf
+from weldx import Q_
+from weldx.asdf.tags.weldx.core.iso_groove import get_groove
 
 linesegment = geo.LineSegment([[0, 0], [2, 1]])
 print(linesegment.__repr__())
@@ -25,6 +27,7 @@ RHTS = geo.RadialHorizontalTraceSegment(3, 90)
 print(RHTS.__repr__())
 print()
 
+
 # how to get this one running?
 # varProfile = geo.VariableProfile([profile], [0], [1])
 # print(varProfile.__repr__())
@@ -35,17 +38,35 @@ LCS = tf.LocalCoordinateSystem(coordinates=[2, 4, -1])
 print(LCS.__repr__())
 print()
 
-# does not show anything - Coordinate system missing?
-# trace = geo.Trace([linesegment, arcsegment], LCS)
-# print(trace.__repr__())
+# create a linear trace segment
+trace_segment = geo.LinearHorizontalTraceSegment(300)
+trace = geo.Trace(trace_segment)
+print(trace.__repr__())
 print()
+
+groove = get_groove(
+    groove_type="VGroove",
+    workpiece_thickness=Q_(0.5, "cm"),
+    groove_angle=Q_(50, "deg"),
+    root_face=Q_(1, "mm"),
+    root_gap=Q_(1, "mm"),
+)
+
+# create 3d workpiece geometry from the groove profile and trace objects
+geometry = geo.Geometry(groove.to_profile(width_default=Q_(4, "mm")), trace)
 
 # Geometry - trace is empty -> no repr?
 # geometry = geo.Geometry(profile, trace)
-# print(geometry.__repr__())
+print(geometry.__repr__())
 print()
+
+# crete a new coordinate system manager with default base coordinate system
+csm = tf.CoordinateSystemManager("base")
+
+# add the workpiece coordinate system
+csm.add_coordinate_system("workpiece", "base", trace.coordinate_system)
 
 # CSM - need to lookup how to initialize this in the tutorial...
 # CSM = tf.CoordinateSystemManager()
-# print(CSM.__repr__())
+print(csm.__repr__())
 print()
