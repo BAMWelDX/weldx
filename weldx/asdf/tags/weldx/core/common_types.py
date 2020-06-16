64afdaacfbd72d8c6a14d02b89b5e502e519163f
Hirthammer <volker.hirthammer@bam.de>
2020-06-16 10:21:32 +0200

    Use dataclasses for asdf common types and add docstrings

--- weldx/asdf/tags/weldx/core/common_types.py
+++ weldx/asdf/tags/weldx/core/common_types.py
@@ -1,17 +1,23 @@
 import numpy as np
+from dataclasses import dataclass
 from pandas.api.types import is_datetime64_any_dtype as is_datetime
+from typing import List
 
 from weldx.asdf.types import WeldxType
 
 
+@dataclass
 class Dimension:
-    def __init__(self, name, length):
-        self.name = name
-        self.length = length
+    """
+    Stores data of a dimension.
+    """
+
+    name: str
+    length: int
 
 
 class DimensionTypeASDF(WeldxType):
-    """Serialization class for a Dimension"""
+    """Serialization class for the 'Dimension' type"""
 
     name = "core/dimension"
     version = "1.0.0"
@@ -21,22 +27,57 @@ class DimensionTypeASDF(WeldxType):
 
     @classmethod
     def to_tree(cls, node: Dimension, ctx):
-        """Convert an xarray.Dataset to a tagged tree"""
+        """
+        Convert an instance of the 'Dimension' type into YAML representations.
+
+        Parameters
+        ----------
+        node :
+            Instance of the 'Dimension' type to be serialized.
+
+        ctx :
+            An instance of the 'AsdfFile' object that is being written out.
+
+        Returns
+        -------
+            A basic YAML type ('dict', 'list', 'str', 'int', 'float', or
+            'complex') representing the properties of the 'Dimension' type to be
+            serialized.
+
+        """
         tree = {"name": node.name, "length": node.length}
 
         return tree
 
     @classmethod
     def from_tree(cls, tree, ctx):
-        """Convert a tagged tree to an xarray.Dataset"""
+        """
+        Converts basic types representing YAML trees into custom types.
+
+        Parameters
+        ----------
+        tree :
+            An instance of a basic Python type (possibly nested) that
+            corresponds to a YAML subtree.
+        ctx :
+            An instance of the 'AsdfFile' object that is being constructed.
+
+        Returns
+        -------
+        Dimension :
+            An instance of the 'Dimension' type.
+
+        """
         return Dimension(tree["name"], tree["length"])
 
 
+@dataclass
 class Variable:
-    def __init__(self, name, dimensions, data: np.ndarray):
-        self.name = name
-        self.dimensions = dimensions
-        self.data = data
+    """Represents an n-dimensional piece of data."""
+
+    name: str
+    dimensions: List
+    data: np.ndarray
 
 
 class VariableTypeASDF(WeldxType):
@@ -50,13 +91,47 @@ class VariableTypeASDF(WeldxType):
 
     @staticmethod
     def convert_time_dtypes(data: np.ndarray):
+        """
+        Convert time format data types to a corresponding numeric data type.
+
+        If the data's type isn't a time format, the function returns the unmodified
+        data.
+
+        Parameters
+        ----------
+        data :
+            Data that should be converted.
+
+        Returns
+        -------
+        np.ndarray :
+            Unmodified or converted data.
+
+        """
         if is_datetime(data.dtype):
             return data.astype(np.int64)
         return data
 
     @classmethod
     def to_tree(cls, node: Variable, ctx):
-        """Convert an xarray.Dataset to a tagged tree"""
+        """
+        Convert an instance of the 'Variable' type into YAML representations.
+
+        Parameters
+        ----------
+        node :
+            Instance of the 'Variable' type to be serialized.
+
+        ctx :
+            An instance of the 'AsdfFile' object that is being written out.
+
+        Returns
+        -------
+            A basic YAML type ('dict', 'list', 'str', 'int', 'float', or
+            'complex') representing the properties of the 'Variable' type to be
+            serialized.
+
+        """
         dtype = node.data.dtype.str
         data = cls.convert_time_dtypes(data=node.data)
         tree = {
@@ -70,7 +145,23 @@ class VariableTypeASDF(WeldxType):
 
     @classmethod
     def from_tree(cls, tree, ctx):
-        """Convert a tagged tree to an xarray.Dataset"""
+        """
+        Converts basic types representing YAML trees into custom types.
+
+        Parameters
+        ----------
+        tree :
+            An instance of a basic Python type (possibly nested) that
+            corresponds to a YAML subtree.
+        ctx :
+            An instance of the 'AsdfFile' object that is being constructed.
+
+        Returns
+        -------
+        Variable :
+            An instance of the 'Variable' type.
+
+        """
         dtype = np.dtype(tree["dtype"])
         data = np.array(tree["data"]).astype(dtype)
         return Variable(tree["name"], tree["dimensions"], data)
