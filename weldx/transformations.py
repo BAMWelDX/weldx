1cebcbfd4a72f834604ff78696d682437c43e7cb
Hirthammer <volker.hirthammer@bam.de>
2020-07-02 09:41:34 +0200

    Add details to get_local_coosrdinate_system docstring

--- weldx/transformations.py
+++ weldx/transformations.py
@@ -999,6 +999,25 @@ class CoordinateSystemManager:
     ) -> LocalCoordinateSystem:
         """Get a coordinate system in relation to another reference system.
 
+        If any coordinate system that is involved in the coordinate transformation has
+        a time dependency, the union of all coordinate systems' timestamps is
+        determined. Subsequently, all coordinate systems are interpolated in time to
+        match the time union before they are used in the transformation. The purpose is
+        to prevent interpolation errors when rotations are involved. In result, the
+        returned 'LocalCoordinateSystem' timestamps equals the time union of all
+        coordinate systems that were used during the transformation.
+
+        Without the preceding time union interpolation, multiple subsequent
+        transformations can cause changes in the rotation direction and transform
+        circular movements into linear ones. The reason for this is that each individual
+        transformation using the 'LocalCoordinateSystem's + operator generates a new
+        ''set of keyframes'' for the next transformation. Those ''keyframes'' do not
+        preserve the rotational trajectory of a coordinate system attached to a rotating
+        parent system. Additionally, the rotation angle between two keyframes after a
+        transformation might exceed 180°. Since the employed SLERP method always uses
+        the smaller rotation angle, subsequent interpolations will cause a change of the
+        rotation order.
+
         Parameters
         ----------
         coordinate_system_name :
