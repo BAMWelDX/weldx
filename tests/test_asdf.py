891eba90f9d2272897c26ea58caadf21b4ed3989
Hirthammer <volker.hirthammer@bam.de>
2020-07-03 11:02:41 +0200

    Fix SeepSource issue

--- tests/test_asdf.py
+++ tests/test_asdf.py
@@ -10,7 +10,6 @@ import pandas as pd
 import pytest
 
 from weldx.asdf.extension import WeldxAsdfExtension, WeldxExtension
-
 # weld design -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.aws.design.base_metal import BaseMetal
 from weldx.asdf.tags.weldx.aws.design.connection import Connection
@@ -19,7 +18,6 @@ from weldx.asdf.tags.weldx.aws.design.sub_assembly import SubAssembly
 from weldx.asdf.tags.weldx.aws.design.weld_details import WeldDetails
 from weldx.asdf.tags.weldx.aws.design.weldment import Weldment
 from weldx.asdf.tags.weldx.aws.design.workpiece import Workpiece
-
 # welding process -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.aws.process.arc_welding_process import ArcWeldingProcess
 from weldx.asdf.tags.weldx.aws.process.gas_component import GasComponent
@@ -27,10 +25,8 @@ from weldx.asdf.tags.weldx.aws.process.shielding_gas_for_procedure import (
     ShieldingGasForProcedure,
 )
 from weldx.asdf.tags.weldx.aws.process.shielding_gas_type import ShieldingGasType
-
 # iso groove -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.core.iso_groove import get_groove
-
 # validators -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.debug.test_shape_validator import ShapeValidatorTestClass
 from weldx.asdf.tags.weldx.debug.validator_testclass import ValidatorTestClass
@@ -395,8 +391,7 @@ def test_shape_validator_syntax():
         res = _custom_shape_validator({"shape": list_test}, list_expected)
         if isinstance(res, dict):
             return True
-        else:
-            return False
+        return False
 
     # correct evaluation
     assert val([3], [3])
