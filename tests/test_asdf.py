639562ff4885dcb05cc6b9a532f209f07ea010f2
Hirthammer <volker.hirthammer@bam.de>
2020-07-03 10:57:53 +0200

    Fix pydocstyle issue

--- tests/test_asdf.py
+++ tests/test_asdf.py
@@ -10,6 +10,7 @@ import pandas as pd
 import pytest
 
 from weldx.asdf.extension import WeldxAsdfExtension, WeldxExtension
+
 # weld design -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.aws.design.base_metal import BaseMetal
 from weldx.asdf.tags.weldx.aws.design.connection import Connection
@@ -18,6 +19,7 @@ from weldx.asdf.tags.weldx.aws.design.sub_assembly import SubAssembly
 from weldx.asdf.tags.weldx.aws.design.weld_details import WeldDetails
 from weldx.asdf.tags.weldx.aws.design.weldment import Weldment
 from weldx.asdf.tags.weldx.aws.design.workpiece import Workpiece
+
 # welding process -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.aws.process.arc_welding_process import ArcWeldingProcess
 from weldx.asdf.tags.weldx.aws.process.gas_component import GasComponent
@@ -25,8 +27,10 @@ from weldx.asdf.tags.weldx.aws.process.shielding_gas_for_procedure import (
     ShieldingGasForProcedure,
 )
 from weldx.asdf.tags.weldx.aws.process.shielding_gas_type import ShieldingGasType
+
 # iso groove -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.core.iso_groove import get_groove
+
 # validators -----------------------------------------------------------------
 from weldx.asdf.tags.weldx.debug.test_shape_validator import ShapeValidatorTestClass
 from weldx.asdf.tags.weldx.debug.validator_testclass import ValidatorTestClass
@@ -387,7 +391,7 @@ def test_shape_validator_syntax():
     """Test handling of custom shape validation syntax in Python."""
 
     def val(list_test, list_expected):
-        """Wrapper to add shape key to lists."""
+        """Add shape key to lists."""
         res = _custom_shape_validator({"shape": list_test}, list_expected)
         if isinstance(res, dict):
             return True
