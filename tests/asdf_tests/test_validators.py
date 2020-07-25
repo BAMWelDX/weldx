54110ea21961fb9d3f74d70ea113bfb6a3dfc7d3
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-07-25 21:34:48 +0200

    parametrize test_validators.py

--- tests/asdf_tests/test_validators.py
+++ tests/asdf_tests/test_validators.py
@@ -1,4 +1,3 @@
-import jsonschema
 import numpy as np
 import pandas as pd
 import pytest
@@ -13,21 +12,23 @@ from weldx.asdf.validators import _custom_shape_validator
 from .utility import _write_read_buffer
 
 
-def test_property_tag_validator():
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        PropertyTagTestClass(),
+        pytest.param(
+            PropertyTagTestClass(prop3=pd.Timedelta(2, "s")),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+        pytest.param(
+            PropertyTagTestClass(prop3="STRING"),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+    ],
+)
+def test_property_tag_validator(test_input):
     """Test custom ASDF shape validators."""
-    test = PropertyTagTestClass()
-    tree = {"root_node": test}
-    _write_read_buffer(tree)
-
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        test = PropertyTagTestClass(prop3=pd.Timedelta(2, "s"))
-        tree = {"root_node": test}
-        _write_read_buffer(tree)
-
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        test = PropertyTagTestClass(prop3="STRING")
-        tree = {"root_node": test}
-        _write_read_buffer(tree)
+    _write_read_buffer({"root_node": test_input})
 
 
 def test_shape_validator_syntax():
@@ -120,40 +121,12 @@ def test_shape_validator_syntax():
         _custom_shape_validator(1.0, [2])
 
 
-def test_shape_validator():
-    """Test custom ASDF shape validators."""
-    test = ShapeValidatorTestClass(
-        prop1=np.ones((1, 2, 3)),
-        prop2=np.ones((3, 2, 1)),
-        prop3=np.ones((2, 4, 6, 8, 10)),
-        prop4=np.ones((1, 3, 5, 7, 9)),
-        prop5=3.141,
-        nested_prop={"p1": np.ones((10, 8, 6, 4, 2)), "p2": np.ones((9, 7, 5, 3, 1))},
-    )
-    tree = {"root_node": test}
-    _write_read_buffer(tree)
-    # test_read = data["root_node"]
-    # TODO: add value assertion
-
-    with pytest.raises(ValidationError):
-        test = ShapeValidatorTestClass(
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        ShapeValidatorTestClass(
             prop1=np.ones((1, 2, 3)),
             prop2=np.ones((3, 2, 1)),
-            prop3=np.ones((2, 4, 6, 8, 10)),
-            prop4=np.ones((2, 3, 5, 7, 9)),  # mismatch a with prop5
-            prop5=3.141,
-            nested_prop={
-                "p1": np.ones((10, 8, 6, 4, 2)),
-                "p2": np.ones((9, 7, 5, 3, 1)),
-            },
-        )
-        tree = {"root_node": test}
-        _write_read_buffer(tree)
-
-    with pytest.raises(ValidationError):
-        test = ShapeValidatorTestClass(
-            prop1=np.ones((1, 2, 3)),
-            prop2=np.ones((5, 2, 1)),  # mismatch n with prop1
             prop3=np.ones((2, 4, 6, 8, 10)),
             prop4=np.ones((1, 3, 5, 7, 9)),
             prop5=3.141,
@@ -161,36 +134,116 @@ def test_shape_validator():
                 "p1": np.ones((10, 8, 6, 4, 2)),
                 "p2": np.ones((9, 7, 5, 3, 1)),
             },
-        )
-        tree = {"root_node": test}
-        _write_read_buffer(tree)
+        ),
+        pytest.param(
+            ShapeValidatorTestClass(
+                prop1=np.ones((1, 2, 3)),
+                prop2=np.ones((3, 2, 1)),
+                prop3=np.ones((2, 4, 6, 8, 10)),
+                prop4=np.ones((2, 3, 5, 7, 9)),  # mismatch a with prop5
+                prop5=3.141,
+                nested_prop={
+                    "p1": np.ones((10, 8, 6, 4, 2)),
+                    "p2": np.ones((9, 7, 5, 3, 1)),
+                },
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+        pytest.param(
+            ShapeValidatorTestClass(
+                prop1=np.ones((1, 2, 3)),
+                prop2=np.ones((5, 2, 1)),  # mismatch n with prop1
+                prop3=np.ones((2, 4, 6, 8, 10)),
+                prop4=np.ones((1, 3, 5, 7, 9)),
+                prop5=3.141,
+                nested_prop={
+                    "p1": np.ones((10, 8, 6, 4, 2)),
+                    "p2": np.ones((9, 7, 5, 3, 1)),
+                },
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+        pytest.param(
+            ShapeValidatorTestClass(
+                prop1=np.ones((1, 2, 3)),
+                prop2=np.ones((3, 2, 1)),
+                prop3=np.ones((2, 4, 6, 8, 10)),
+                prop4=np.ones((1, 3, 5, 7, 9)),
+                prop5=3.141,
+                nested_prop={"p1": np.ones((10, 8, 6, 4, 2))},  # missing p2
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+    ],
+)
+def test_shape_validator(test_input):
+    _write_read_buffer({"root": test_input})
 
-    with pytest.raises(ValidationError):
-        test = ShapeValidatorTestClass(
-            prop1=np.ones((1, 2, 3)),
-            prop2=np.ones((3, 2, 1)),
-            prop3=np.ones((2, 4, 6, 8, 10)),
-            prop4=np.ones((1, 3, 5, 7, 9)),
-            prop5=3.141,
-            nested_prop={"p1": np.ones((10, 8, 6, 4, 2))},
-        )
-        tree = {"root_node": test}
-        _write_read_buffer(tree)
 
-
-def test_unit_validator():
-    """Test custom ASDF validators."""
-    test = UnitValidatorTestClass(
-        length_prop=Q_(1, "inch"),
-        velocity_prop=Q_(2, "km / s"),
-        current_prop=Q_(np.eye(2, 2), "mA"),
-        nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
-        simple_prop={"value": float(3), "unit": "m"},
-    )
-
-    tree = {"root_node": test}
-
-    data = _write_read_buffer(tree)
+@pytest.mark.parametrize(
+    "test",
+    [
+        UnitValidatorTestClass(
+            length_prop=Q_(1, "inch"),
+            velocity_prop=Q_(2, "km / s"),
+            current_prop=Q_(np.eye(2, 2), "mA"),
+            nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
+            simple_prop={"value": float(3), "unit": "m"},
+        ),
+        pytest.param(
+            UnitValidatorTestClass(
+                length_prop=Q_(1, "s"),  # wrong unit
+                velocity_prop=Q_(2, "km / s"),
+                current_prop=Q_(np.eye(2, 2), "mA"),
+                nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
+                simple_prop={"value": float(3), "unit": "m"},
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+        pytest.param(
+            UnitValidatorTestClass(
+                length_prop=Q_(1, "s"),
+                velocity_prop=Q_(2, "liter"),  # wrong unit
+                current_prop=Q_(np.eye(2, 2), "mA"),
+                nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
+                simple_prop={"value": float(3), "unit": "m"},
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+        pytest.param(
+            UnitValidatorTestClass(
+                length_prop=Q_(1, "inch"),
+                velocity_prop=Q_(2, "km / s"),
+                current_prop=Q_(np.eye(2, 2), "V"),  # wrong unit
+                nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
+                simple_prop={"value": float(3), "unit": "m"},
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+        pytest.param(
+            UnitValidatorTestClass(
+                length_prop=Q_(1, "m"),
+                velocity_prop=Q_(2, "km / s"),
+                current_prop=Q_(np.eye(2, 2), "mA"),
+                nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "V")),  # wrong unit
+                simple_prop={"value": float(3), "unit": "m"},
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+        pytest.param(
+            UnitValidatorTestClass(
+                length_prop=Q_(1, "m"),
+                velocity_prop=Q_(2, "km / s"),
+                current_prop=Q_(np.eye(2, 2), "mA"),
+                nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
+                simple_prop={"value": float(3), "unit": "s"},  # wrong unit
+            ),
+            marks=pytest.mark.xfail(raises=ValidationError),
+        ),
+    ],
+)
+def test_unit_validator(test):
+    data = _write_read_buffer({"root_node": test})
     test_read = data["root_node"]
     assert isinstance(data, dict)
     assert test_read.length_prop == test.length_prop
@@ -199,58 +252,3 @@ def test_unit_validator():
     assert np.all(test_read.nested_prop["q1"] == test.nested_prop["q1"])
     assert test_read.nested_prop["q2"] == test.nested_prop["q2"]
     assert test_read.simple_prop == test.simple_prop
-
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        test = UnitValidatorTestClass(
-            length_prop=Q_(1, "s"),  # wrong unit
-            velocity_prop=Q_(2, "km / s"),
-            current_prop=Q_(np.eye(2, 2), "mA"),
-            nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
-            simple_prop={"value": float(3), "unit": "m"},
-        )
-        tree = {"root_node": test}
-        data = _write_read_buffer(tree)
-
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        test = UnitValidatorTestClass(
-            length_prop=Q_(1, "s"),
-            velocity_prop=Q_(2, "liter"),  # wrong unit
-            current_prop=Q_(np.eye(2, 2), "mA"),
-            nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
-            simple_prop={"value": float(3), "unit": "m"},
-        )
-        tree = {"root_node": test}
-        data = _write_read_buffer(tree)
-
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        test = UnitValidatorTestClass(
-            length_prop=Q_(1, "inch"),
-            velocity_prop=Q_(2, "km / s"),
-            current_prop=Q_(np.eye(2, 2), "V"),  # wrong unit
-            nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
-            simple_prop={"value": float(3), "unit": "m"},
-        )
-        tree = {"root_node": test}
-        data = _write_read_buffer(tree)
-
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        test = UnitValidatorTestClass(
-            length_prop=Q_(1, "m"),
-            velocity_prop=Q_(2, "km / s"),
-            current_prop=Q_(np.eye(2, 2), "mA"),
-            nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "V")),  # wrong unit
-            simple_prop={"value": float(3), "unit": "m"},
-        )
-        tree = {"root_node": test}
-        data = _write_read_buffer(tree)
-
-    with pytest.raises(jsonschema.exceptions.ValidationError):
-        test = UnitValidatorTestClass(
-            length_prop=Q_(1, "m"),
-            velocity_prop=Q_(2, "km / s"),
-            current_prop=Q_(np.eye(2, 2), "mA"),
-            nested_prop=dict(q1=Q_(np.eye(3, 3), "m"), q2=Q_(2, "m^3")),
-            simple_prop={"value": float(3), "unit": "s"},  # wrong unit
-        )
-        tree = {"root_node": test}
-        data = _write_read_buffer(tree)
