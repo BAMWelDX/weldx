3cd1125182688d0375d45cc54cffda5ab93e0d6b
Hirthammer <volker.hirthammer@bam.de>
2020-07-28 16:49:28 +0200

    Refactor TimeSeries comparison tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,12 +3,14 @@
 
 import numpy as np
 import pandas as pd
+import pint
 import pytest
 from xarray import DataArray
 
 from weldx.constants import WELDX_QUANTITY as Q_
 from weldx.constants import WELDX_UNIT_REGISTRY as UREG
 from weldx.core import MathematicalExpression, TimeSeries
+
 
 # Todo: Move this to conftest.py?
 def get_test_name(param):
@@ -207,259 +209,150 @@ class TestMathematicalExpression:
 # --------------------------------------------------------------------------------------
 
 
-def test_time_series_construction():
-    """Test the construction of the TimeSeries class."""
-    # single value ----------------------------------------
-    value = Q_(1, "m")
-    ts_constant = TimeSeries(data=value)
+class TestTimeSeries:
+    """Tests for the TimeSeries class."""
 
-    assert ts_constant.data == value
-    assert ts_constant.time is None
-    assert ts_constant.interpolation is None
-    assert ts_constant.shape == (1,)
-    assert value.check(UREG.get_dimensionality(ts_constant.units))
-
-    exp_data_array_constant = DataArray(
-        data=Q_([1], "m"),
-        dims=["time"],
-        coords={"time": pd.TimedeltaIndex([0])},
-        attrs={"interpolation": None},
-    )
-    assert ts_constant.data_array.identical(exp_data_array_constant)
-
-    # discrete values -------------------------------------
-    time = pd.TimedeltaIndex([0, 1, 2, 3, 4], unit="s")
-    values = Q_(np.array([10, 11, 12, 14, 16]), "mm")
-    ts_discrete = TimeSeries(data=values, time=time, interpolation="step")
-
-    assert np.all(ts_discrete.time == time)
-    assert np.all(ts_discrete.data == values)
-    assert ts_discrete.interpolation == "step"
-    assert ts_discrete.shape == (5,)
-    assert values.check(UREG.get_dimensionality(ts_discrete.units))
-
-    exp_data_array_discrete = DataArray(
-        data=values,
-        dims=["time"],
-        coords={"time": time},
-        attrs={"interpolation": "step"},
-    )
-    assert ts_discrete.data_array.identical(exp_data_array_discrete)
-
-    # discrete values - pint time quantity ----------------
-    time_pint = Q_([0, 1, 2, 3, 4], "s")
-    values = Q_(np.array([10, 11, 12, 14, 16]), "mm")
-    ts_discrete_pint = TimeSeries(data=values, time=time_pint, interpolation="step")
-
-    assert np.all(ts_discrete_pint.time == time)
-    assert np.all(ts_discrete_pint.data == values)
-    assert ts_discrete_pint.interpolation == "step"
-    assert ts_discrete_pint.shape == (5,)
-    assert values.check(UREG.get_dimensionality(ts_discrete_pint.units))
-
-    assert ts_discrete_pint.data_array.identical(exp_data_array_discrete)
-
-    # mathematical expression -----------------------------
-    # scalar
-    expr_string = "a*t+b"
-    parameters = {"a": Q_(2, "1/s"), "b": Q_(-2, "")}
-    expr = MathematicalExpression(expression=expr_string, parameters=parameters)
-
-    ts_expr = TimeSeries(data=expr)
-
-    assert ts_expr.time is None
-    assert ts_expr.interpolation is None
-    assert ts_expr.shape == (1,)
-    assert ts_expr.data_array is None
-
-    assert isinstance(ts_expr.data, MathematicalExpression)
-    assert ts_expr.data.num_variables == 1
-    assert ts_expr.data.num_parameters == 2
-    assert ts_expr.data.get_variable_names()[0] == "t"
-    assert parameters["b"].check(UREG.get_dimensionality(ts_expr.units))
-
-    for parameter in parameters:
-        assert parameter in ts_expr.data.parameters
-        assert parameters[parameter] == ts_expr.data.parameters[parameter]
-
-    # vector
-    expr_string_vec = "a * time + b"
-    parameters_vec = {"a": Q_([[2, 3, 4]], "m/s"), "b": Q_([[-2, 3, 1]], "m")}
-    expr_vec = MathematicalExpression(
-        expression=expr_string_vec, parameters=parameters_vec
-    )
-
-    ts_expr_vec = TimeSeries(data=expr_vec)
-
-    assert ts_expr_vec.time is None
-    assert ts_expr_vec.interpolation is None
-    assert ts_expr_vec.shape == (1, 3)
-    assert ts_expr_vec.data_array is None
-
-    assert isinstance(ts_expr_vec.data, MathematicalExpression)
-    assert ts_expr_vec.data.num_variables == 1
-    assert ts_expr_vec.data.num_parameters == 2
-    assert ts_expr_vec.data.get_variable_names()[0] == "time"
-    assert parameters_vec["b"].check(UREG.get_dimensionality(ts_expr_vec.units))
-
-    for parameter in parameters_vec:
-        assert parameter in ts_expr_vec.data.parameters
-        assert np.all(
-            parameters_vec[parameter] == ts_expr_vec.data.parameters[parameter]
-        )
-
-    # exceptions ------------------------------------------
-    # invalid interpolation
-    with pytest.raises(ValueError):
-        TimeSeries(values, time, "super interpolator 2000")
-    with pytest.raises(ValueError):
-        TimeSeries(values, time, None)
-    # invalid time component
-    with pytest.raises(ValueError):
-        TimeSeries(values, values.magnitude)
-
-    # too many free variables
-    with pytest.raises(Exception):
-        expr_2 = MathematicalExpression(expression=expr_string, parameters={})
-        TimeSeries(data=expr_2)
-    # incompatible parameter units
-    with pytest.raises(Exception):
-        expr_3 = MathematicalExpression(
-            expression=expr_string, parameters={"a": Q_(2, "1/s"), "b": Q_(-2, "m")}
-        )
-        TimeSeries(data=expr_3)
-    # cannot be evaluated with time vectors
-    with pytest.raises(Exception):
-        expr_4 = MathematicalExpression(
-            expression=expr_string,
-            parameters={"a": Q_([2, 3, 4], "1/s"), "b": Q_([-2, 3, 1], "")},
-        )
-        TimeSeries(data=expr_4)
-
-
-def test_time_series_comparison():
-    """Test the comparison functions of the TimeSeries class."""
-    value_constant = Q_(1, "m")
-    ts_constant = TimeSeries(data=value_constant)
+    ME = MathematicalExpression
+    TDI = pd.TimedeltaIndex
+    TS = TimeSeries
 
     time_discrete = pd.TimedeltaIndex([0, 1, 2, 3, 4], unit="s")
+    value_constant = Q_(1, "m")
     values_discrete = Q_(np.array([10, 11, 12, 14, 16]), "mm")
-    ts_discrete = TimeSeries(
-        data=values_discrete, time=time_discrete, interpolation="step"
+    me_expr_str = "a*t + b"
+    me_params = {"a": Q_(2, "m/s"), "b": Q_(-2, "m")}
+
+    ts_constant = TimeSeries(value_constant)
+    ts_discrete = TimeSeries(values_discrete, time_discrete, "step")
+    ts_expr = TimeSeries(ME(me_expr_str, me_params))
+
+    # tests ----------------------------------------------------------------------------
+
+    @pytest.mark.parametrize(
+        "data, time, interpolation, shape_exp",
+        [
+            (Q_(1, "m"), None, None, (1,)),
+            (Q_([3, 7, 1], "m"), TDI([0, 1, 2], unit="s"), "step", (3,)),
+            (Q_([3, 7, 1], ""), Q_([0, 1, 2], "s"), "step", (3,)),
+        ],
     )
+    def test_construction_discrete(self, data, time, interpolation, shape_exp):
+        """Test the construction of the TimeSeries class."""
+        # set expected values
+        if isinstance(time, pint.Quantity):
+            time_exp = pd.TimedeltaIndex(time.magnitude, unit="s")
+        else:
+            time_exp = time
 
-    expr_string = "a*t+b"
-    parameters = {"a": Q_(2, "1/s"), "b": Q_(-2, "")}
-    expr = MathematicalExpression(expression=expr_string, parameters=parameters)
+        # create instance
+        ts = TimeSeries(data=data, time=time, interpolation=interpolation)
 
-    ts_expr = TimeSeries(data=expr)
+        # check
+        assert np.all(ts.data == data)
+        assert np.all(ts.time == time_exp)
+        assert ts.interpolation == interpolation
+        assert ts.shape == shape_exp
+        assert data.check(UREG.get_dimensionality(ts.units))
 
-    # comparison with duplicate ---------------------------
+        assert np.all(ts.data_array.data == data)
+        assert ts.data_array.attrs["interpolation"] == interpolation
+        if time_exp is None:
+            assert "time" not in ts.data_array
+        else:
+            assert np.all(ts.data_array.time == time_exp)
 
-    assert ts_constant == TimeSeries(value_constant)
-    assert ts_discrete == TimeSeries(values_discrete, time_discrete, "step")
-    assert ts_expr == TimeSeries(expr)
+    # ----------------------------------------------------------------------------------
 
-    assert not ts_constant != TimeSeries(value_constant)
-    assert not ts_discrete != TimeSeries(values_discrete, time_discrete, "step")
-    assert not ts_expr != TimeSeries(expr)
+    params_scalar = {"a": Q_(2, "1/s"), "b": Q_(-2, "")}
+    params_vec = {"a": Q_([[2, 3, 4]], "m/s"), "b": Q_([[-2, 3, 1]], "m")}
 
-    # comparison against each other -----------------------
+    @pytest.mark.parametrize(
+        "data,  shape_exp, unit_exp",
+        [
+            (ME("a*t + b", params_scalar), (1,), ""),
+            (ME("a*t + b", params_vec), (1, 3), "m"),
+        ],
+    )
+    def test_construction_expression(self, data, shape_exp, unit_exp):
+        """Test the construction of the TimeSeries class."""
+        ts = TimeSeries(data=data)
 
-    assert not ts_constant == ts_discrete
-    assert not ts_constant == ts_expr
-    assert not ts_discrete == ts_expr
+        # check
+        assert ts.data == data
+        assert ts.time is None
+        assert ts.interpolation is None
+        assert ts.shape == shape_exp
+        assert ts.data_array is None
+        assert Q_(1, unit_exp).check(UREG.get_dimensionality(ts.units))
 
-    assert ts_constant != ts_discrete
-    assert ts_constant != ts_expr
-    assert ts_discrete != ts_expr
+    # -----------------------------------------------------
 
-    # comparison with other type --------------------------
+    values_def = Q_([5, 7, 3, 6, 8], "m")
+    time_def = Q_([0, 1, 2, 3, 4], "s")
+    me_too_many_vars = ME("a*t + b", {})
+    me_param_units = ME("a*t + b", {"a": Q_(2, "1/s"), "b": Q_(-2, "m")})
+    me_time_vec = ME("a*t + b", {"a": Q_([2, 3, 4], "1/s"), "b": Q_([-2, 3, 1], "")})
 
-    assert not ts_constant == 1
-    assert not ts_discrete == 1
-    assert not ts_expr == 1
+    @pytest.mark.parametrize(
+        "data, time, interpolation, exception_type, test_name",
+        [
+            (values_def, time_def, "int", ValueError, "# unknown interpolation"),
+            (values_def, time_def, None, ValueError, "# wrong interp. parameter type"),
+            (values_def, time_def.magnitude, "step", ValueError, "# invalid time type"),
+            (me_too_many_vars, None, None, Exception, "# too many free variables"),
+            (me_param_units, None, None, Exception, "# incompatible parameter units"),
+            (me_time_vec, None, None, Exception, "# not compatible with time vectors"),
+        ],
+        ids=get_test_name,
+    )
+    def test_construction_exceptions(
+        self, data, time, interpolation, exception_type, test_name
+    ):
+        """Test the exceptions of the 'set_parameter' method."""
+        with pytest.raises(exception_type):
+            TimeSeries(data=data, time=time, interpolation=interpolation)
 
-    assert ts_constant != 1
-    assert ts_discrete != 1
-    assert ts_expr != 1
+    # test_comparison ------------------------------------------------------------------
 
-    assert not ts_constant == "nope"
-    assert not ts_discrete == "nope"
-    assert not ts_expr == "nope"
-
-    assert ts_constant != "nope"
-    assert ts_discrete != "nope"
-    assert ts_expr != "nope"
-
-    # constant value specific mismatches ------------------
-    ts_constant_value_wrong = TimeSeries(Q_(1337, "m"))
-    assert not ts_constant == ts_constant_value_wrong
-    assert ts_constant != ts_constant_value_wrong
-
-    ts_constant_unit_wrong = TimeSeries(Q_(1, "s"))
-    assert not ts_constant == ts_constant_unit_wrong
-    assert ts_constant != ts_constant_unit_wrong
-
-    ts_constant_unit_prefix_wrong = TimeSeries(Q_(1, "mm"))
-    assert not ts_constant == ts_constant_unit_prefix_wrong
-    assert ts_constant != ts_constant_unit_prefix_wrong
-
-    # discrete value specific mismatches ------------------
-    time_wrong = pd.TimedeltaIndex([0, 1, 2, 3, 5])
-    ts_discrete_time_wrong = TimeSeries(values_discrete, time_wrong, "step")
-    assert not ts_discrete == ts_discrete_time_wrong
-    assert ts_discrete != ts_discrete_time_wrong
-
+    time_wrong_values = TDI([0, 1, 2, 3, 5], unit="s")
     values_discrete_wrong = Q_(np.array([10, 11, 12, 15, 16]), "mm")
-    ts_discrete_values_wrong = TimeSeries(values_discrete_wrong, time_discrete, "step")
-    assert not ts_discrete == ts_discrete_values_wrong
-    assert ts_discrete != ts_discrete_values_wrong
-
     values_unit_wrong = Q_(np.array([10, 11, 12, 14, 16]), "s")
-    ts_discrete_unit_wrong = TimeSeries(values_unit_wrong, time_discrete, "step")
-    assert not ts_discrete == ts_discrete_unit_wrong
-    assert ts_discrete != ts_discrete_unit_wrong
-
     values_unit_prefix_wrong = Q_(np.array([10, 11, 12, 14, 16]), "m")
-    ts_discrete_unit_prefix_wrong = TimeSeries(
-        values_unit_prefix_wrong, time_discrete, "step"
+    params_wrong_values = {"a": Q_(2, "1/s"), "b": Q_(-1, "")}
+    params_wrong_unit = {"a": Q_(2, "g/s"), "b": Q_(-2, "g")}
+    params_wrong_unit_prefix = {"a": Q_(2, "m/ms"), "b": Q_(-2, "m")}
+
+    @pytest.mark.parametrize(
+        "ts, ts_other, result_exp",
+        [
+            (ts_constant, TS(value_constant), True),
+            (ts_discrete, TS(values_discrete, time_discrete, "step"), True),
+            (ts_expr, TS(ME(me_expr_str, me_params)), True),
+            (ts_constant, ts_discrete, False),
+            (ts_constant, ts_expr, False),
+            (ts_discrete, ts_expr, False),
+            (ts_constant, 1, False),
+            (ts_discrete, 1, False),
+            (ts_expr, 1, False),
+            (ts_constant, "wrong", False),
+            (ts_discrete, "wrong", False),
+            (ts_expr, "wrong", False),
+            (ts_constant, TS(Q_(1337, "m")), False),
+            (ts_constant, TS(Q_(1, "mm")), False),
+            (ts_constant, TS(Q_(1, "s")), False),
+            (ts_discrete, TS(values_discrete, time_wrong_values, "step"), False),
+            (ts_discrete, TS(values_discrete_wrong, time_discrete, "step"), False),
+            (ts_discrete, TS(values_unit_prefix_wrong, time_discrete, "step"), False),
+            (ts_discrete, TS(values_discrete, time_discrete, "linear"), False),
+            (ts_expr, TS(ME("a*t + 2*b", me_params)), False),
+            (ts_expr, TS(ME(me_expr_str, params_wrong_values)), False),
+            (ts_expr, TS(ME(me_expr_str, params_wrong_unit)), False),
+            (ts_expr, TS(ME(me_expr_str, params_wrong_unit_prefix)), False),
+        ],
     )
-    assert not ts_discrete == ts_discrete_unit_prefix_wrong
-    assert ts_discrete != ts_discrete_unit_prefix_wrong
-
-    ts_discrete_interp_wrong = TimeSeries(values_discrete, time_discrete, "linear")
-    assert not ts_discrete == ts_discrete_interp_wrong
-    assert ts_discrete != ts_discrete_interp_wrong
-
-    # expression specific mismatches ----------------------
-
-    expr_wrong_exp = MathematicalExpression("a*t+ 2*b", parameters)
-    ts_expr_wrong_expr = TimeSeries(expr_wrong_exp)
-    assert not ts_expr == ts_expr_wrong_expr
-    assert ts_expr != ts_expr_wrong_expr
-
-    parameters_wrong_values = {"a": Q_(2, "1/s"), "b": Q_(-1, "")}
-    expr_wrong_param_values = MathematicalExpression(
-        expr_string, parameters_wrong_values
-    )
-    ts_expr_wrong_param_values = TimeSeries(expr_wrong_param_values)
-    assert not ts_expr == ts_expr_wrong_param_values
-    assert ts_expr != ts_expr_wrong_param_values
-
-    parameters_wrong_unit = {"a": Q_(2, "m/s"), "b": Q_(-2, "m")}
-    expr_wrong_param_unit = MathematicalExpression(expr_string, parameters_wrong_unit)
-    ts_expr_wrong_param_unit = TimeSeries(expr_wrong_param_unit)
-    assert not ts_expr == ts_expr_wrong_param_unit
-    assert ts_expr != ts_expr_wrong_param_unit
-
-    parameters_wrong_unit_prefix = {"a": Q_(2, "1/ms"), "b": Q_(-2, "")}
-    expr_wrong_param_unit_prefix = MathematicalExpression(
-        expr_string, parameters_wrong_unit_prefix
-    )
-    ts_expr_wrong_param_unit_prefix = TimeSeries(expr_wrong_param_unit_prefix)
-    assert not ts_expr == ts_expr_wrong_param_unit_prefix
-    assert ts_expr != ts_expr_wrong_param_unit_prefix
+    def test_comparison(self, ts, ts_other, result_exp):
+        assert (ts == ts_other) is result_exp
+        assert (ts != ts_other) is not result_exp
 
 
 def test_time_series_interp_time_constant():
