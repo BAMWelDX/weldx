f7db64bececa1540f382f4154666477233e2e95c
Nikitha-ramasetti <38511466+Nikitha-ramasetti@users.noreply.github.com>
2020-09-01 15:44:35 +0200

    Documentation setup readthedocs (#115)
    Add minimal readthedocs setup

--- doc/conf.py
+++ doc/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath(''))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,6 +24,10 @@ author = 'BAM'
 # The full version, including alpha/beta/rc tags
 release = '0.0.2'
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
+
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -31,7 +35,22 @@ release = '0.0.2'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "recommonmark",
+    "sphinx.ext.autodoc",
 ]
+
+# The suffix of source filenames.
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+# The encoding of source files.
+# source_encoding = 'utf-8-sig'
+
+# The master toctree document.
+master_doc = "index"
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,3 +72,13 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "WelDX_notext.svg"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#html_theme_options = {"logo_only": True}
+
---
+++ doc/index.rst
@@ -0,0 +1,19 @@
+
+
+Welcome to WelDX documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   contents
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
