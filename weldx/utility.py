9808bc9c34720fb7d731fe9f931ab28c577a1221
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-10-21 10:31:45 +0200

    fix bug converting pint.Quantities

--- weldx/utility.py
+++ weldx/utility.py
@@ -168,9 +168,9 @@ def to_pandas_time_index(
 
     if isinstance(time, pint.Quantity):
         base = "s"  # using low base unit could cause rounding errors
-        if not np.iterable(time):
+        if not np.iterable(time):  # catch zero-dim arrays
             time = np.expand_dims(time, 0)
-            return pd.TimedeltaIndex(data=time.to(base).magnitude, unit=base)
+        return pd.TimedeltaIndex(data=time.to(base).magnitude, unit=base)
 
     if isinstance(time, (xr.DataArray, xr.Dataset)):
         if "time" in time.coords:
