03bf994b0b45c20c9385db75db5a5bc518b2f702
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2020-10-29 10:07:27 +0100

    add missing mappings and default groove creation function

--- weldx/asdf/tags/weldx/groove/iso_9692_1.py
+++ weldx/asdf/tags/weldx/groove/iso_9692_1.py
@@ -49,6 +49,8 @@ class IGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["1.2.1", "1.2.2", "2.1"])
 
+    _mapping = dict(t="workpiece_thickness", b="root_gap")
+
 
 @ureg_check_class("[length]", "[]", "[length]", "[length]", None)
 @dataclass
@@ -79,12 +81,9 @@ class VGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["1.3", "1.5"])
 
-    _mapping = {
-        "t": "workpiece_thickness",
-        "alpha": "groove_angle",
-        "b": "root_gap",
-        "c": "root_face",
-    }
+    _mapping = dict(
+        t="workpiece_thickness", alpha="groove_angle", b="root_gap", c="root_face",
+    )
 
 
 @ureg_check_class("[length]", "[]", "[]", "[length]", "[length]", "[length]", None)
@@ -122,6 +121,15 @@ class VVGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["1.7"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        alpha="groove_angle",
+        beta="bevel_angle",
+        b="root_gap",
+        c="root_face",
+        h="root_face2",
+    )
+
 
 @ureg_check_class("[length]", "[]", "[]", "[length]", "[length]", "[length]", None)
 @dataclass
@@ -158,6 +166,15 @@ class UVGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["1.6"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        alpha="groove_angle",
+        beta="bevel_angle",
+        R="bevel_radius",
+        b="root_gap",
+        h="root_face",
+    )
+
 
 @ureg_check_class("[length]", "[]", "[length]", "[length]", "[length]", None)
 @dataclass
@@ -191,6 +208,14 @@ class UGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["1.8"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        beta="bevel_angle",
+        R="bevel_radius",
+        b="root_gap",
+        c="root_face",
+    )
+
 
 @ureg_check_class("[length]", "[]", "[length]", "[length]", None)
 @dataclass
@@ -220,6 +245,10 @@ class HVGroove(IsoBaseGroove):
     c: pint.Quantity = Q_(0, "mm")
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["1.9.1", "1.9.2", "2.8"])
+
+    _mapping = dict(
+        t="workpiece_thickness", beta="bevel_angle", b="root_gap", c="root_face"
+    )
 
 
 @ureg_check_class("[length]", "[]", "[length]", "[length]", "[length]", None)
@@ -254,10 +283,16 @@ class HUGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["1.11", "2.10"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        beta="bevel_angle",
+        R="bevel_radius",
+        b="root_gap",
+        c="root_face",
+    )
 
-@ureg_check_class(
-    "[length]", "[]", "[]", "[length]", "[length]", "[length]", "[length]", None
-)
+
+@ureg_check_class("[length]", "[]", "[]", "[length]", None, None, "[length]", None)
 @dataclass
 class DVGroove(IsoBaseGroove):
     # noinspection PyUnresolvedReferences
@@ -295,6 +330,16 @@ class DVGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["2.4", "2.5.1", "2.5.2"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        alpha_1="groove_angle",
+        alpha_2="groove_angle2",
+        b="root_gap",
+        c="root_face",
+        h1="root_face2",
+        h2="root_face3",
+    )
+
     def __post_init__(self):
         if self.h1 is None and self.h2 is None:
             self.h1 = (self.t - self.c) / 2
@@ -312,8 +357,8 @@ class DVGroove(IsoBaseGroove):
     "[length]",
     "[length]",
     "[length]",
-    "[length]",
-    "[length]",
+    None,
+    None,
     "[length]",
     None,
 )
@@ -360,6 +405,18 @@ class DUGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["2.7"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        beta_1="bevel_angle",
+        beta_2="bevel_angle2",
+        R="bevel_radius",
+        R2="bevel_radius2",
+        b="root_gap",
+        c="root_face",
+        h1="root_face2",
+        h2="root_face3",
+    )
+
     def __post_init__(self):
         if self.h1 is None and self.h2 is None:
             self.h1 = (self.t - self.c) / 2
@@ -370,9 +427,7 @@ class DUGroove(IsoBaseGroove):
             self.h1 = self.h2
 
 
-@ureg_check_class(
-    "[length]", "[]", "[]", "[length]", "[length]", "[length]", "[length]", None
-)
+@ureg_check_class("[length]", "[]", "[]", "[length]", None, None, "[length]", None)
 @dataclass
 class DHVGroove(IsoBaseGroove):
     # noinspection PyUnresolvedReferences
@@ -410,6 +465,16 @@ class DHVGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["2.9.1", "2.9.2"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        beta_1="bevel_angle",
+        beta_2="bevel_angle2",
+        c="root_face",
+        h1="root_face2",
+        h2="root_face3",
+        b="root_gap",
+    )
+
     def __post_init__(self):
         if self.h1 is None and self.h2 is None:
             self.h1 = (self.t - self.c) / 2
@@ -427,8 +492,8 @@ class DHVGroove(IsoBaseGroove):
     "[length]",
     "[length]",
     "[length]",
-    "[length]",
-    "[length]",
+    None,
+    None,
     "[length]",
     None,
 )
@@ -475,6 +540,18 @@ class DHUGroove(IsoBaseGroove):
     b: pint.Quantity = Q_(0, "mm")
     code_number: List[str] = field(default_factory=lambda: ["2.11"])
 
+    _mapping = dict(
+        t="workpiece_thickness",
+        beta_1="bevel_angle",
+        beta_2="bevel_angle2",
+        R="bevel_radius",
+        R2="bevel_radius2",
+        b="root_gap",
+        c="root_face",
+        h1="root_face2",
+        h2="root_face3",
+    )
+
     def __post_init__(self):
         if self.h1 is None and self.h2 is None:
             self.h1 = (self.t - self.c) / 2
@@ -486,7 +563,7 @@ class DHUGroove(IsoBaseGroove):
 
 
 @ureg_check_class(
-    "[length]", "[length]", "[]", "[length]", "[length]", None,
+    "[length]", None, None, None, None, None,
 )
 @dataclass
 class FFGroove(IsoBaseGroove):
@@ -519,6 +596,15 @@ class FFGroove(IsoBaseGroove):
     code_number: str = None
     b: pint.Quantity = None
     e: pint.Quantity = None
+
+    _mapping = dict(
+        t_1="workpiece_thickness",
+        t_2="workpiece_thickness2",
+        alpha="groove_angle",
+        b="root_gap",
+        e="special_depth",
+        code_number="code_number",
+    )
 
 
 def _get_class_from_tag(instance_tag: str):
@@ -558,6 +644,7 @@ def get_groove(
 
     # convert function arguments to groove arguments
     args = {k: _loc[v] for k, v in _mapping.items() if _loc[v] is not None}
+    args["code_number"] = _loc["code_number"]
 
     return groove_cls(**args)
 
@@ -597,3 +684,218 @@ class IsoGrooveType(WeldxType):
         groove_name = _get_class_from_tag(tree._tag)
         groove = _groove_name_to_type[groove_name](**tree)
         return groove
+
+
+def _create_test_grooves():
+    """Create dictionary with examples for all groove variations."""
+    v_groove = get_groove(
+        groove_type="VGroove",
+        workpiece_thickness=Q_(9, "mm"),
+        groove_angle=Q_(50, "deg"),
+        root_face=Q_(4, "mm"),
+        root_gap=Q_(2, "mm"),
+    )
+    u_groove = get_groove(
+        groove_type="UGroove",
+        workpiece_thickness=Q_(15, "mm"),
+        bevel_angle=Q_(9, "deg"),
+        bevel_radius=Q_(6, "mm"),
+        root_face=Q_(3, "mm"),
+        root_gap=Q_(1, "mm"),
+    )
+    i_groove = get_groove(
+        groove_type="IGroove", workpiece_thickness=Q_(4, "mm"), root_gap=Q_(4, "mm")
+    )
+    uv_groove = get_groove(
+        groove_type="UVGroove",
+        workpiece_thickness=Q_(12, "mm"),
+        groove_angle=Q_(60, "deg"),
+        bevel_angle=Q_(11, "deg"),
+        bevel_radius=Q_(6, "mm"),
+        root_face=Q_(4, "mm"),
+        root_gap=Q_(2, "mm"),
+    )
+    vv_groove = get_groove(
+        groove_type="VVGroove",
+        workpiece_thickness=Q_(12, "mm"),
+        groove_angle=Q_(70, "deg"),
+        bevel_angle=Q_(13, "deg"),
+        root_gap=Q_(3, "mm"),
+        root_face=Q_(1, "mm"),
+        root_face2=Q_(5, "mm"),
+    )
+    hv_groove = get_groove(
+        groove_type="HVGroove",
+        workpiece_thickness=Q_(9, "mm"),
+        bevel_angle=Q_(55, "deg"),
+        root_gap=Q_(2, "mm"),
+        root_face=Q_(1, "mm"),
+    )
+    hu_groove = get_groove(
+        groove_type="HUGroove",
+        workpiece_thickness=Q_(18, "mm"),
+        bevel_angle=Q_(15, "deg"),
+        bevel_radius=Q_(8, "mm"),
+        root_gap=Q_(2, "mm"),
+        root_face=Q_(3, "mm"),
+    )
+    dv_groove = get_groove(
+        groove_type="DVGroove",
+        workpiece_thickness=Q_(19, "mm"),
+        groove_angle=Q_(40, "deg"),
+        groove_angle2=Q_(60, "deg"),
+        root_gap=Q_(2, "mm"),
+        root_face=Q_(5, "mm"),
+        root_face2=Q_(7, "mm"),
+        root_face3=Q_(7, "mm"),
+    )
+    dv_groove2 = get_groove(
+        groove_type="DVGroove",
+        workpiece_thickness=Q_(19, "mm"),
+        groove_angle=Q_(40, "deg"),
+        groove_angle2=Q_(60, "deg"),
+        root_gap=Q_(2, "mm"),
+        root_face=Q_(5, "mm"),
+    )
+    dv_groove3 = get_groove(
+        groove_type="DVGroove",
+        workpiece_thickness=Q_(19, "mm"),
+        groove_angle=Q_(40, "deg"),
+        groove_angle2=Q_(60, "deg"),
+        root_gap=Q_(2, "mm"),
+        root_face=Q_(5, "mm"),
+        root_face3=Q_(7, "mm"),
+    )
+    # DU grooves
+    du_groove = get_groove(
+        groove_type="DUGroove",
+        workpiece_thickness=Q_(33, "mm"),
+        bevel_angle=Q_(8, "deg"),
+        bevel_angle2=Q_(12, "deg"),
+        bevel_radius=Q_(6, "mm"),
+        bevel_radius2=Q_(6, "mm"),
+        root_face=Q_(3, "mm"),
+        root_face2=Q_(15, "mm"),
+        root_gap=Q_(2, "mm"),
+    )
+    du_groove2 = get_groove(
+        groove_type="DUGroove",
+        workpiece_thickness=Q_(33, "mm"),
+        bevel_angle=Q_(8, "deg"),
+        bevel_angle2=Q_(12, "deg"),
+        bevel_radius=Q_(6, "mm"),
+        bevel_radius2=Q_(6, "mm"),
+        root_face=Q_(3, "mm"),
+        root_gap=Q_(2, "mm"),
+    )
+    du_groove3 = get_groove(
+        groove_type="DUGroove",
+        workpiece_thickness=Q_(33, "mm"),
+        bevel_angle=Q_(8, "deg"),
+        bevel_angle2=Q_(12, "deg"),
+        bevel_radius=Q_(6, "mm"),
+        bevel_radius2=Q_(6, "mm"),
+        root_face=Q_(3, "mm"),
+        root_face2=Q_(15, "mm"),
+        root_face3=Q_(15, "mm"),
+        root_gap=Q_(2, "mm"),
+    )
+    du_groove4 = get_groove(
+        groove_type="DUGroove",
+        workpiece_thickness=Q_(33, "mm"),
+        bevel_angle=Q_(8, "deg"),
+        bevel_angle2=Q_(12, "deg"),
+        bevel_radius=Q_(6, "mm"),
+        bevel_radius2=Q_(6, "mm"),
+        root_face=Q_(3, "mm"),
+        root_face3=Q_(15, "mm"),
+        root_gap=Q_(2, "mm"),
+    )
+    dhv_groove = get_groove(
+        groove_type="DHVGroove",
+        workpiece_thickness=Q_(11, "mm"),
+        bevel_angle=Q_(35, "deg"),
+        bevel_angle2=Q_(60, "deg"),
+        root_face2=Q_(5, "mm"),
+        root_face=Q_(1, "mm"),
+        root_gap=Q_(3, "mm"),
+    )
+    dhu_groove = get_groove(
+        groove_type="DHUGroove",
+        workpiece_thickness=Q_(32, "mm"),
+        bevel_angle=Q_(10, "deg"),
+        bevel_angle2=Q_(20, "deg"),
+        bevel_radius=Q_(8, "mm"),
+        bevel_radius2=Q_(8, "mm"),
+        root_face2=Q_(15, "mm"),
+        root_face=Q_(2, "mm"),
+        root_gap=Q_(2, "mm"),
+    )
+    ff_groove0 = get_groove(
+        groove_type="FFGroove", workpiece_thickness=Q_(5, "mm"), code_number="1.12",
+    )
+    ff_groove1 = get_groove(
+        groove_type="FFGroove",
+        workpiece_thickness=Q_(5, "mm"),
+        workpiece_thickness2=Q_(7, "mm"),
+        groove_angle=Q_(80, "deg"),
+        root_gap=Q_(1, "mm"),
+        code_number="3.1.1",
+    )
+    ff_groove2 = get_groove(
+        groove_type="FFGroove",
+        workpiece_thickness=Q_(2, "mm"),
+        workpiece_thickness2=Q_(5, "mm"),
+        root_gap=Q_(1, "mm"),
+        code_number="3.1.2",
+    )
+    ff_groove3 = get_groove(
+        groove_type="FFGroove",
+        workpiece_thickness=Q_(2, "mm"),
+        workpiece_thickness2=Q_(5, "mm"),
+        groove_angle=Q_(80, "deg"),
+        root_gap=Q_(1, "mm"),
+        code_number="3.1.3",
+    )
+    ff_groove4 = get_groove(
+        groove_type="FFGroove",
+        workpiece_thickness=Q_(2, "mm"),
+        workpiece_thickness2=Q_(5, "mm"),
+        groove_angle=Q_(80, "deg"),
+        special_depth=Q_(4, "mm"),
+        code_number="4.1.2",
+    )
+    ff_groove5 = get_groove(
+        groove_type="FFGroove",
+        workpiece_thickness=Q_(2, "mm"),
+        workpiece_thickness2=Q_(5, "mm"),
+        root_gap=Q_(1, "mm"),
+        code_number="4.1.3",
+    )
+
+    test_data = dict(
+        v_groove=(v_groove, VGroove),
+        u_groove=(u_groove, UGroove),
+        i_groove=(i_groove, IGroove),
+        uv_groove=(uv_groove, UVGroove),
+        vv_groove=(vv_groove, VVGroove),
+        hv_groove=(hv_groove, HVGroove),
+        hu_groove=(hu_groove, HUGroove),
+        dv_groove=(dv_groove, DVGroove),
+        dv_groove2=(dv_groove2, DVGroove),
+        dv_groove3=(dv_groove3, DVGroove),
+        du_groove=(du_groove, DUGroove),
+        du_groove2=(du_groove2, DUGroove),
+        du_groove3=(du_groove3, DUGroove),
+        du_groove4=(du_groove4, DUGroove),
+        dhv_groove=(dhv_groove, DHVGroove),
+        dhu_groove=(dhu_groove, DHUGroove),
+        ff_groove0=(ff_groove0, FFGroove),
+        ff_groove1=(ff_groove1, FFGroove),
+        ff_groove2=(ff_groove2, FFGroove),
+        ff_groove3=(ff_groove3, FFGroove),
+        ff_groove4=(ff_groove4, FFGroove),
+        ff_groove5=(ff_groove5, FFGroove),
+    )
+
+    return test_data
