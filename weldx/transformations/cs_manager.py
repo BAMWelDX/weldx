b9816022fb1c54af1c4e7e8641deda363a5a9cdb
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-03-04 18:53:59 +0100

    test sphinx warning

--- weldx/transformations/cs_manager.py
+++ weldx/transformations/cs_manager.py
@@ -22,6 +22,7 @@ from .local_cs import LocalCoordinateSystem
 # only import heavy-weight packages on type checking
 if TYPE_CHECKING:
     import matplotlib.axes
+    import matplotlib
     from scipy.spatial.transform import Rotation as Rot
 
 _DEFAULT_LEN_UNIT = UREG.millimeters
