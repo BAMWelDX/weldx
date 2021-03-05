8761c7600d28f94593d6faab4d1ec2752bbc9b1b
marscher <martin.scherer@bam.de>
2021-03-05 17:56:57 +0100

    no duplicates

--- doc/conf.py
+++ doc/conf.py
@@ -199,7 +199,10 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["**.ipynb_checkpoints"]
+exclude_patterns = ["**.ipynb_checkpoints",
+                    "**/cs_manager/*",
+                    "**/local_cs/*",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
