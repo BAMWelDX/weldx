4804a81c17f080654c665cce947e0f7b6f69ee59
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-03-09 14:06:14 +0100

    remove seam_length from compute_welding_speed

--- weldx/welding/utility.py
+++ weldx/welding/utility.py
@@ -1,16 +1,15 @@
 """Collection of welding utilities."""
-import pint
 import numpy as np
+import pint
 
 from weldx.constants import WELDX_UNIT_REGISTRY
 from weldx.welding.groove.iso_9692_1 import IsoBaseGroove
 
 
-@WELDX_UNIT_REGISTRY.check(None, "[length]", "[length]/[time]", "[length]")
+@WELDX_UNIT_REGISTRY.check(None, "[length]/[time]", "[length]")
 # TODO: extend the ckeck decorator to check on output! # ret='[length]/[time]')
 def compute_welding_speed(
     groove: IsoBaseGroove,
-    seam_length: pint.Quantity,
     wire_feed: pint.Quantity,
     wire_diameter: pint.Quantity,
 ):
@@ -20,8 +19,6 @@ def compute_welding_speed(
     ----------
     groove
         groove definition to compute welding speed for.
-    seam_length: pint.Quantity["length"]
-        length of the seam
     wire_feed: pint.Quantity["length/time"]
         feed of the wire
     wire_diameter: pint.Quantity["length"]
@@ -35,7 +32,7 @@ def compute_welding_speed(
     """
     groove_area = groove.cross_sect_area
     wire_area = np.pi / 4 * wire_diameter ** 2
-    weld_speed = wire_area * wire_feed / groove_area * seam_length
+    weld_speed = wire_area * wire_feed / groove_area
 
     weld_speed.ito_reduced_units()
     return weld_speed
