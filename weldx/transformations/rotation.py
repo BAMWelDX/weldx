6cb9bedd822dc53d7d9214d626267147fa95dd34
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-03-16 13:25:23 +0100

    WXRotation: derive docstrings from parent class.

--- weldx/transformations/rotation.py
+++ weldx/transformations/rotation.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.spatial.transform import Rotation as _Rotation
 
 from weldx.constants import WELDX_UNIT_REGISTRY as UREG
+from weldx.util import inherit_docstrings
 
 _DEFAULT_LEN_UNIT = UREG.millimeters
 _DEFAULT_ANG_UNIT = UREG.rad
@@ -63,45 +64,30 @@ def rotation_matrix_z(angle) -> np.ndarray:
     return _Rotation.from_euler("z", angle).as_matrix()
 
 
+@inherit_docstrings
 class WXRotation(_Rotation):
     """Wrapper for creating meta-tagged Scipy.Rotation objects."""
 
     @classmethod
     def from_quat(cls, quat: np.ndarray) -> "WXRotation":
-        """Initialize from quaternions.
-
-        scipy.spatial.transform.Rotation docs for details.
-        """
         rot = super().from_quat(quat)
         setattr(rot, "wx_meta", {"constructor": "from_quat"})
         return rot
 
     @classmethod
     def from_matrix(cls, matrix: np.ndarray) -> "WXRotation":
-        """Initialize from matrix.
-
-        scipy.spatial.transform.Rotation docs for details.
-        """
         rot = super().from_matrix(matrix)
         setattr(rot, "wx_meta", {"constructor": "from_matrix"})
         return rot
 
     @classmethod
     def from_rotvec(cls, rotvec: np.ndarray) -> "WXRotation":
-        """Initialize from rotation vector.
-
-        scipy.spatial.transform.Rotation docs for details.
-        """
         rot = super().from_rotvec(rotvec)
         setattr(rot, "wx_meta", {"constructor": "from_rotvec"})
         return rot
 
     @classmethod
     def from_euler(cls, seq: str, angles, degrees: bool = False) -> "WXRotation":
-        """Initialize from euler angles.
-
-        scipy.spatial.transform.Rotation docs for details.
-        """
         rot = super().from_euler(seq=seq, angles=angles, degrees=degrees)
         setattr(
             rot,
