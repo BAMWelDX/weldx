3ca79a0af32edeac6ffe69d793d771afefc0c770
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-04-09 13:54:13 +0200

    fix

--- weldx/measurement.py
+++ weldx/measurement.py
@@ -18,6 +18,9 @@ class Data:
     data: xr.DataArray  # skipcq: PTC-W0052
 
     def __eq__(self, other):
+        """Check for equality with other object."""
+        if not isinstance(other, Data):
+            return False
         return self.name == other.name and self.data.identical(other.data)
 
 
