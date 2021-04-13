fb9e08af2999b4f96bc84b72b2782ad4223fe8f7
vhirtham <volker.hirthammer@bam.de>
2021-04-13 12:37:19 +0200

    Remove old asdf serialization class

--- weldx/asdf/tags/weldx/measurement/measurement_chain.py
+++ weldx/asdf/tags/weldx/measurement/measurement_chain.py
@@ -11,27 +11,6 @@ __all__ = [
 class MeasurementChainType(WeldxType):
     """Serialization class for measurement chains"""
 
-    name = "measurement/measurement_chain"
-    version = "1.0.0"
-    types = [MeasurementChain]
-    requires = ["weldx"]
-    handle_dynamic_subclasses = True
-
-    @classmethod
-    def to_tree(cls, node: MeasurementChain, ctx):
-        """convert to tagged tree and remove all None entries from node dictionary"""
-        tree = node.__dict__
-        return tree
-
-    @classmethod
-    def from_tree(cls, tree, ctx):
-        obj = MeasurementChain(**tree)
-        return obj
-
-
-class MeasurementChainGraphType(WeldxType):
-    """Serialization class for measurement chains"""
-
     name = "measurement/measurement_chain_graph"
     version = "1.0.0"
     types = [MeasurementChain]
