151372997755f97efa309724987c5237a18a9e55
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-04-15 07:50:41 +0200

    Update scripts/clean_notebooks.py
    Co-authored-by: vhirtham <volker.hirthammer@gmail.com>

--- scripts/clean_notebooks.py
+++ scripts/clean_notebooks.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     from weldx.util import _clean_notebook_ids
 
     p = Path(__file__).parents[1]
-    print(p)
     notebooks = p.glob("tutorials/*.ipynb")
     for nb in notebooks:
         _clean_notebook_ids(nb)
