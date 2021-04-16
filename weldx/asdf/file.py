b004da40541ad3355cda83b2740a243919ebb8bb
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-04-16 16:28:41 +0200

    fix some tests, added sync (wrapper for update) to avoid conflict with dict.update

--- weldx/asdf/file.py
+++ weldx/asdf/file.py
@@ -40,6 +40,7 @@ class SupportsFileReadWrite(Protocol):
 
 
 types_file_like = Union[IOBase, SupportsFileReadOnly, SupportsFileReadWrite]
+types_path_and_file_like = Union[str, pathlib.Path, types_file_like]
 
 
 class WeldxFile(UserDict):
@@ -53,10 +54,12 @@ class WeldxFile(UserDict):
         reading or reading/writing mode: "r" or "rw"
     asdf_args :
         see `asdf.open` for reference.
+    tree :
+        an optional dictionary to write to the file.
     sync :
         If True, the changes to file will be written upon closing this. This is only
         relevant, if the file has been opened in write mode.
-    history_software_entry :
+    software_history_entry :
         a optional dictionary which will be used to add history entries upon
         modification of the file. It has to provide the following keys:
         ("name", "author", "homepage", "version")
@@ -66,10 +69,11 @@ class WeldxFile(UserDict):
     def __init__(
         self,
         filename_or_file_like: Union[str, pathlib.Path, types_file_like],
-        mode="r",
-        asdf_args=None,
-        sync=True,
-        history_software_entry=None,
+        mode: str = "r",
+        asdf_args: Mapping = None,
+        tree: Mapping = None,
+        sync: bool = True,
+        software_history_entry: Mapping = None,
     ):
         if asdf_args is None:
             asdf_args = {}
@@ -88,15 +92,24 @@ class WeldxFile(UserDict):
         self.sync = bool(sync)
 
         # let asdf.open handle/raise exceptions
-        self._asdf_handle: asdf.AsdfFile = asdf.open(
-            filename_or_file_like,
-            mode=mode,
-            extensions=[WeldxExtension(), WeldxAsdfExtension()],
-            **asdf_args,
-        )
-        super(WeldxFile, self).__init__(dict=self._asdf_handle.tree)
+        if tree:
+            asdf_file = AsdfFile(tree=tree)
+            asdf_file.write_to(filename_or_file_like, **asdf_args)
+        else:
+            asdf_file = asdf.open(
+                filename_or_file_like,
+                mode=mode,
+                extensions=[WeldxExtension(), WeldxAsdfExtension()],
+                **asdf_args,
+            )
+        self._asdf_handle: asdf.AsdfFile = asdf_file
 
-        if history_software_entry is None:
+        # UserDict interface, we want to store a reference to the tree, but the ctor
+        # of UserDict takes a copy, so we do it manually here.
+        super(WeldxFile, self).__init__()
+        self.data = self._asdf_handle.tree
+
+        if software_history_entry is None:
             from weldx import __version__ as version
 
             self._DEFAULT_SOFTWARE_ENTRY = {
@@ -105,6 +118,8 @@ class WeldxFile(UserDict):
                 "homepage": "https://www.bam.de/Content/EN/Projects/WelDX/weldx.html",
                 "version": version,
             }
+        else:
+            self._DEFAULT_SOFTWARE_ENTRY = software_history_entry
 
     @classmethod
     def from_tree(cls, tree: dict, **asdf_kwargs):
@@ -129,6 +144,9 @@ class WeldxFile(UserDict):
             self._asdf_handle.update()
         self._asdf_handle.close()
 
+    # TODO: this updates the tree, but should it also be synched to disk directly?
+    # this could be inefficient if lots of updates are performed,
+    # but also adds more safety.
     #    def update(self, __m: Mapping[_KT, _VT], **kwargs: _VT) -> None: ...
     def update(
         self,
@@ -146,7 +164,78 @@ class WeldxFile(UserDict):
         except BaseException as e:
             print(e)
             self.add_history_entry(change_desc, software=software)
-            self._asdf_handle.update()
+
+    def sync(
+        self,
+        all_array_storage=None,
+        all_array_compression="input",
+        auto_inline=None,
+        pad_blocks=False,
+        include_block_index=True,
+        version=None,
+    ):
+        """
+        Update the file on disk in place.
+
+        Parameters
+        ----------
+        all_array_storage : string, optional
+            If provided, override the array storage type of all blocks
+            in the file immediately before writing.  Must be one of:
+
+            - ``internal``: The default.  The array data will be
+              stored in a binary block in the same ASDF file.
+
+            - ``external``: Store the data in a binary block in a
+              separate ASDF file.
+
+            - ``inline``: Store the data as YAML inline in the tree.
+
+        all_array_compression : string, optional
+            If provided, set the compression type on all binary blocks
+            in the file.  Must be one of:
+
+            - ``''`` or `None`: No compression.
+
+            - ``zlib``: Use zlib compression.
+
+            - ``bzp2``: Use bzip2 compression.
+
+            - ``lz4``: Use lz4 compression.
+
+            - ``input``: Use the same compression as in the file read.
+              If there is no prior file, acts as None
+
+        auto_inline : int, optional
+            When the number of elements in an array is less than this
+            threshold, store the array as inline YAML, rather than a
+            binary block.  This only works on arrays that do not share
+            data with other arrays.  Default is 0.
+
+        pad_blocks : float or bool, optional
+            Add extra space between blocks to allow for updating of
+            the file.  If `False` (default), add no padding (always
+            return 0).  If `True`, add a default amount of padding of
+            10% If a float, it is a factor to multiple content_size by
+            to get the new total size.
+
+        include_block_index : bool, optional
+            If `False`, don't include a block index at the end of the
+            file.  (Default: `True`)  A block index is never written
+            if the file has a streamed block.
+
+        version : str, optional
+            The ASDF version to write out.  If not provided, it will
+            write out in the latest version supported by asdf.
+        """
+        self._asdf_handle.update(
+            all_array_storage=all_array_storage,
+            all_array_compression=all_array_compression,
+            auto_inline=auto_inline,
+            pad_blocks=pad_blocks,
+            include_block_index=include_block_index,
+            version=version,
+        )
 
     def add_history_entry(self, change_desc: str, software: dict = None) -> None:
         """adds an history_entry to the file.
@@ -171,16 +260,11 @@ class WeldxFile(UserDict):
     def quality_standard(self) -> Optional[str]:
         return self._quality_standard
 
-    # dict interface
-    #def keys(self):
-    #    return self._asdf_handle.keys()
-
-    #def __getitem__(self, key):
-    #    return self._asdf_handle[key]
-
     def __setitem__(self, key, value):
         # FIXME: this only handles top level write access! So we got to wrap the ASDFEntries?
-        # FIXME: is not called upon weldxfile['foo'] = 'bar' ...
+        # FIXME: is not called upon weldxfile['foo'] = 'bar' ...#
+        # so better make it explicit, if users change something, they are responsible
+        # to add a proper history entry.
         import textwrap
 
         short_value_str = textwrap.shorten(str(value), 30)
@@ -188,30 +272,17 @@ class WeldxFile(UserDict):
         self.add_history_entry(change_desc)
         self._asdf_handle[key] = value
 
-    # def __contains__(self, item):
-    #     return item in self._asdf_handle
-
     def __delitem__(self, item):
         del self._asdf_handle.tree[item]
         self.add_history_entry(f"deleted key {item}")
         self._asdf_handle.update()
 
-    #def __iter__(self):
-    #    return iter(self._asdf_handle.tree)
-
-    #def __len__(self):
-    #    return len(self._asdf_handle.tree)
-
     @property
+    # TODO: should we actually expose this? It allows advanced operations for adults.
     def file_handle(self) -> IOBase:
+        if self._asdf_handle._closed:
+            raise RuntimeError("closed file, cannot access file handle.")
         return self._asdf_handle._fd._fd
-
-    def copy_to_buffer(self) -> BytesIO:
-        res = BytesIO()
-        self._asdf_handle.write_to(res)
-        res.seek(0)
-
-        return res
 
     @property
     def as_attr(self):
@@ -219,14 +290,15 @@ class WeldxFile(UserDict):
 
         return attrdict.AttrDict(self)
 
-    def write_to(self, fn: Optional[str] = None, **asdf_args):
+    def write_to(self, fd: Optional[types_path_and_file_like] = None, **asdf_args):
         """write current weldx file to given file name.
-
 
         Parameters
         ----------
-        fn :
-            When no file name is given, write to a buffer and return it.
+        fd : str, pathlib.Path or file-like object
+            May be a string path to a file, or a Python file-like
+            object.  If a string path, the file is automatically
+            closed after writing.
 
         asdf_args :
             Allowed parameters:
@@ -242,15 +314,17 @@ class WeldxFile(UserDict):
         -------
         The given input file name or a buffer, in case the input was omitted.
         """
-        if fn is None:
-            fn = BytesIO()
+        created = False
+        if fd is None:
+            fd = BytesIO()
+            created = True
 
         # TODO: if no args are given, should we use the args given in ctor?
-        self._asdf_handle.write_to(fn, **asdf_args)
+        self._asdf_handle.write_to(fd, **asdf_args)
 
-        if fn is not None:
-            fn.seek(0)
-        return fn
+        if created:
+            fd.seek(0)
+        return fd
 
     # def _repr_html_(self) -> Optional[str]:
     #    from weldx.asdf.util import notebook_fileprinter
