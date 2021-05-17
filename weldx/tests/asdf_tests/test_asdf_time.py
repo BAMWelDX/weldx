36a202e92279a704e42f33e4e6fe36cd604b519e
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-05-17 13:22:58 +0200

    fix

--- weldx/tests/asdf_tests/test_asdf_time.py
+++ weldx/tests/asdf_tests/test_asdf_time.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from weldx.asdf.util import write_read_buffer
+from weldx.asdf.util import write_buffer, write_read_buffer
 
 
 @pytest.mark.parametrize(
@@ -29,7 +29,7 @@ def test_time_classes(inputs):
 def test_time_classes_max_inline():
     # test support for 64bit literals
     dti = pd.DatetimeIndex(["2020-01-01", "2020-01-02", "2020-01-04", "2020-01-05"])
-    _write_buffer(
+    write_buffer(
         {"root": dti},
         write_kwargs={"all_array_storage": "inline"},
     )
