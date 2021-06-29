38366645ebf19917f54ffabc44cd19a1a04d9c21
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-06-29 11:24:25 +0200

    add test

--- weldx/tests/asdf_tests/test_asdf_base_types.py
+++ weldx/tests/asdf_tests/test_asdf_base_types.py
@@ -1,6 +1,7 @@
 """Tests asdf implementations of python base types."""
 import uuid
-
+import xarray as xr
+import numpy as np
 import pytest
 from asdf import ValidationError
 
@@ -16,3 +17,14 @@ def test_uuid():
 
     with pytest.raises(ValidationError):
         write_read_buffer({"id": uuid.uuid1()})
+
+
+# --------------------------------------------------------------------------------------
+# xarray.DataSet
+# --------------------------------------------------------------------------------------
+def test_dataset_children():
+    da = xr.DataArray(np.arange(10), name="arr1", attrs={"name": "sample data"})
+    ds = da.to_dataset()
+    ds2 = write_read_buffer({"ds": ds})["ds"]
+    assert ds.arr1.attrs == ds2.arr1.attrs
+    assert np.all(ds.arr1 == ds2.arr1)
