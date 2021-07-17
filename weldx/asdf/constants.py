0af641ef83bad14211864086bc7fe0ebfca439c2
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-07-17 17:49:31 +0200

    update to new uri syntax

--- weldx/asdf/constants.py
+++ weldx/asdf/constants.py
@@ -4,12 +4,12 @@ from pathlib import Path
 from asdf.util import filepath_to_url
 
 SCHEMA_PATH = str(Path(__file__).resolve().parents[0] / "schemas")
-WELDX_SCHEMA_URI_BASE = "http://weldx.bam.de/schemas/"
+WELDX_SCHEMA_URI_BASE = "asdf://weldx.bam.de/weldx/schemas/"
 WELDX_TAG_BASE = "asdf://weldx.bam.de/weldx/tags/"
 WELDX_URL_MAPPING = [
     (
         WELDX_SCHEMA_URI_BASE,
-        filepath_to_url(os.path.join(SCHEMA_PATH, "weldx.bam.de"))
+        filepath_to_url(os.path.join(SCHEMA_PATH, "weldx.bam.de/weldx"))
         + "/{url_suffix}.yaml",
     )
 ]
