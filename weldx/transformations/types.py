40692325238bd7e211ac016fd71830e7c50f83e5
vhirtham <volker.hirthammer@bam.de>
2021-08-06 11:41:27 +0200

    Remove obsolete abstract class

--- weldx/transformations/types.py
+++ weldx/transformations/types.py
@@ -1,6 +1,5 @@
 """shared type definitions."""
-from abc import abstractmethod
-from typing import List, Protocol, Union, runtime_checkable
+from typing import List, Union
 
 import numpy as np
 import xarray as xr
@@ -10,20 +9,7 @@ types_coordinates = Union[xr.DataArray, np.ndarray, List]
 types_orientation = Union[xr.DataArray, np.ndarray, List[List], Rotation]
 
 
-# remove ???
-@runtime_checkable
-class SupportsTime(Protocol):
-    """An ABC with one abstract method time."""
-
-    __slots__ = ()
-
-    @abstractmethod
-    def time(self):  # noqa
-        raise NotImplementedError
-
-
 __all__ = [
-    "SupportsTime",
     "types_coordinates",
     "types_orientation",
 ]
