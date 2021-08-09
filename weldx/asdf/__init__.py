3654c0ad1dc2cb184b2fe92f030aa2eaf5d95dbe
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-08-09 18:37:35 +0200

    refactor new extension file

--- weldx/asdf/__init__.py
+++ weldx/asdf/__init__.py
@@ -3,7 +3,7 @@
 from weldx.asdf import tags  # implement tags before the asdf extensions
 from weldx.asdf import constants, util
 from weldx.asdf._extension import WeldxAsdfExtension, WeldxExtension
-from weldx.asdf.extension_api import get_extensions
+from weldx.asdf.extension import get_extensions
 
 # class imports to weldx.asdf namespace
 from weldx.asdf.tags.weldx.core.file import ExternalFile
