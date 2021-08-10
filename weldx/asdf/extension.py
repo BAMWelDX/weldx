f5c4e553b784f9b6a116c28e7d9a163e8aeb5ca1
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-08-10 14:17:28 +0200

    load extension manifest via DirectoryResourceMapping

--- weldx/asdf/extension.py
+++ weldx/asdf/extension.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import asdf
 from asdf.extension import ManifestExtension
 from asdf.resource import DirectoryResourceMapping
 
@@ -12,12 +11,19 @@ from weldx.asdf.validators import (
 
 from .types import WeldxConverter
 
+
 # RESOURCES ----------------------------------------------------------------------------
-MANIFEST_MAPPING = {
-    "asdf://weldx.bam.de/weldx/extensions/weldx-1.0.0": (
-        Path(__file__).parent / "manifests/weldx_manifest-1.0.0.yaml"
-    ).read_text()
-}
+def get_extension_resource_mapping():
+    # Get path to schemas directory relative to this file
+    schemas_path = Path(__file__).parent / "manifests/"
+    mapping = DirectoryResourceMapping(
+        schemas_path,
+        "asdf://weldx.bam.de/weldx/extensions/",
+        recursive=True,
+        filename_pattern="*.yaml",
+        stem_filename=True,
+    )
+    return mapping
 
 
 def get_schema_resource_mapping():
@@ -34,7 +40,7 @@ def get_schema_resource_mapping():
 
 
 def get_resource_mappings():
-    return [MANIFEST_MAPPING, get_schema_resource_mapping()]
+    return [get_extension_resource_mapping(), get_schema_resource_mapping()]
 
 
 # for mapping in get_resource_mappings():
@@ -46,13 +52,13 @@ class WeldxExtension(ManifestExtension):
     """weldx extension class"""
 
     extension_uri = "asdf://weldx.bam.de/weldx/extensions/weldx-1.0.0"
-    yaml_tag_handles = {"!weldx!": "asdf://weldx.bam.de/weldx/tags/"}
     converters = (cls() for cls in WeldxConverter.__subclasses__())
     # asdf_standard_requirement = ">= 1.2.0, < 1.5.0"
     legacy_class_names = [
         "weldx.asdf.extension.WeldxAsdfExtension",
         "weldx.asdf.extension.WeldxExtension",
     ]
+    yaml_tag_handles = {"!weldx!": "asdf://weldx.bam.de/weldx/tags/"}
     validators = {
         "wx_property_tag": wx_property_tag_validator,
         "wx_unit": wx_unit_validator,
