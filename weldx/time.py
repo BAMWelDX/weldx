6e28dfcbfc4ebbaf5a74ab381521808c85789ef3
vhirtham <volker.hirthammer@bam.de>
2021-08-11 13:02:38 +0200

    Return type hint

--- weldx/time.py
+++ weldx/time.py
@@ -342,7 +342,7 @@ class Time:
         """Use generator to iterate over index values."""
         return (t for t in self.as_pandas_index())
 
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> Union[Timedelta, Timestamp]:
         """Access pandas index."""
         return self.as_pandas_index()[item]
 
