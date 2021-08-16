f342bf389eba1211d33fbdbaa710702a7439a87b
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-08-16 11:52:48 +0200

    finally remove tag from schemas

--- weldx/asdf/__init__.py
+++ weldx/asdf/__init__.py
@@ -1,8 +1,4 @@
 """This submodule contains ASDF related weldx extensions and schemas."""
-# isort:skip_file
-from weldx.asdf import tags  # implement tags before the asdf extensions
-from weldx.asdf import constants, util
-from weldx.asdf.extension import get_extensions, get_resource_mappings
 
-# class imports to weldx.asdf namespace
-from weldx.asdf.tags.weldx.core.file import ExternalFile
+from weldx.asdf import constants, tags, util
+from weldx.asdf.extension import get_extensions, get_resource_mappings
