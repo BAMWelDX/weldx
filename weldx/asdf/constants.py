42b7052790f2f4fe097abfe31610f20543d50a43
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-08-16 22:59:05 +0200

    move schema files again

--- weldx/asdf/constants.py
+++ weldx/asdf/constants.py
@@ -1,6 +1,6 @@
 from weldx.constants import WELDX_PATH
 
-SCHEMA_PATH = WELDX_PATH / "schemas"
+SCHEMA_PATH = WELDX_PATH / "schemas/weldx.bam.de/weldx"
 MANIFEST_PATH = WELDX_PATH / "manifests"
 
 WELDX_URI_BASE = "asdf://weldx.bam.de/weldx/"
