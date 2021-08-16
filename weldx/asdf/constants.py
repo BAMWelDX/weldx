ac6dd453520393a9ce6e8a0e7668fbfb1568a658
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-08-16 14:38:06 +0200

    move schema files and update pytest asdf action

--- weldx/asdf/constants.py
+++ weldx/asdf/constants.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-SCHEMA_PATH = Path(__file__).resolve().parents[1] / "schemas"
+SCHEMA_PATH = Path(__file__).resolve().parents[1] / "schemas/weldx.bam.de/weldx"
 MANIFEST_PATH = Path(__file__).resolve().parents[1] / "manifests"
 
 WELDX_URI_BASE = "asdf://weldx.bam.de/weldx/"
