268cafc46cd37b2d29043c03f7f1a93f06a8aac0
vhirtham <volker.hirthammer@bam.de>
2021-08-20 15:12:33 +0200

    Fix mypy issue

--- devtools/scripts/clean_notebooks.py
+++ devtools/scripts/clean_notebooks.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from pathlib import Path
 
-    from weldx.util import _clean_notebook
+    from weldx.util.util import _clean_notebook
 
     p = Path(__file__).resolve().parent
     notebooks = p.glob("../../tutorials/*.ipynb")
