936db793f33ef0a129da69e6ce49740a23b6a949
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-08-23 18:20:11 +0200

    fix docstring of Q_

--- weldx/constants.py
+++ weldx/constants.py
@@ -2,6 +2,7 @@
 from pathlib import Path as _Path
 
 from pint import UnitRegistry as _ureg
+from pint.quantity import Quantity as _Q_
 
 WELDX_PATH = _Path(__file__).parent.resolve()
 
@@ -16,7 +17,9 @@ WELDX_UNIT_REGISTRY.define("percent = 0.01*count = %")
 WELDX_UNIT_REGISTRY.define("hour = 60*minute = h = hr")
 
 WELDX_QUANTITY = WELDX_UNIT_REGISTRY.Quantity
+WELDX_QUANTITY.__doc__ = _Q_.__doc__  # use docstring from pint Quantity factory.
 Q_ = WELDX_QUANTITY
+del _Q_
 
 __all__ = (
     "WELDX_PATH",
