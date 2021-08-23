697cd6e7c694c68382bf79ec3897343decf243b3
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-08-23 17:52:15 +0200

    emit ImportWarning if doc str could not be derived.

--- weldx/util/util.py
+++ weldx/util/util.py
@@ -177,7 +177,9 @@ def inherit_docstrings(cls):
                 func.__doc__ = getattr(parent, name).__doc__
                 if not func.__doc__:
                     warnings.warn(
-                        f"could not derive docstring for {cls}.{name}", stacklevel=2
+                        f"could not derive docstring for {cls}.{name}",
+                        stacklevel=2,
+                        category=ImportWarning,
                     )
     return cls
 
