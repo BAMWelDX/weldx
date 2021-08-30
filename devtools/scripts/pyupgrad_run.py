e2564dc4f7bdef6aa376489d9a78646abf9cc2f0
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-08-30 11:06:53 +0200

    Update devtools/scripts/pyupgrad_run.py
    Co-authored-by: vhirtham <volker.hirthammer@gmail.com>

--- devtools/scripts/pyupgrad_run.py
+++ devtools/scripts/pyupgrad_run.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     files = Path("./../../weldx/").rglob("*.py")
     for p in files:
-        subprocess.run(
+        subprocess.run(  # skipcq: BAN-B607
             ["pyupgrade", str(p.resolve()), "--py38-plus", "--keep-runtime-typing"],
             check=True,
         )
