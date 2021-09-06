15500f3f8daea4c572d2cf5e169369de4321a1a7
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-09-06 11:43:41 +0200

    let automodule handle this

--- weldx/transformations/__init__.py
+++ weldx/transformations/__init__.py
@@ -1,35 +1,4 @@
-""" Contains methods and classes for coordinate transformations.
-
-.. currentmodule:: weldx.transformations
-
-.. rubric:: Classes
-
-.. autosummary::
-  :toctree:
-  :template: class-template.rst
-  :nosignatures:
-
-  CoordinateSystemManager
-  LocalCoordinateSystem
-  WXRotation
-
-.. rubric:: Functions
-
-.. autosummary::
-   :toctree:
-   :nosignatures:
-
-   scale_matrix
-   normalize
-   orientation_point_plane_containing_origin
-   orientation_point_plane
-   is_orthogonal
-   is_orthogonal_matrix
-   point_left_of_line
-   reflection_sign
-   vector_points_to_left_of_vector
-
-"""
+""" Contains methods and classes for coordinate transformations."""
 from .cs_manager import CoordinateSystemManager
 from .local_cs import LocalCoordinateSystem
 from .rotation import WXRotation
