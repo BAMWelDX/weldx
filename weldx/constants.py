674862f6d902032aff25efbcb1f412ab16cdded0
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-09-07 14:16:15 +0200

    revert constants.py

--- weldx/constants.py
+++ weldx/constants.py
@@ -17,27 +17,6 @@ WELDX_UNIT_REGISTRY.define("hour = 60*minute = h = hr")
 
 WELDX_QUANTITY = WELDX_UNIT_REGISTRY.Quantity
 Q_ = WELDX_QUANTITY
-# Q_.__name__ = "Q_"
-# Q_.__module__ = __name__
-Q_.__doc__ = """Create a quantity from a scalar or array.
-
-The quantity class supports lots of physical units and will combine them during
-mathematical operations
-
-Examples
---------
->>> from weldx import Q_
->>> length = Q_(10, "mm")
->>> length
-
-define a time:
->>> time = Q_(1, "s")
->>> time
-
-lets combine length and time to get a velocity.
->>> v = length / time
->>> v
-"""
 
 __all__ = (
     "WELDX_PATH",
