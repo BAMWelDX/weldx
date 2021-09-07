9121dd4fe067a389f169fffa63eed878d56d3133
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-09-07 14:17:05 +0200

    no explicit listing of transformations content

--- weldx/transformations/__init__.py
+++ weldx/transformations/__init__.py
@@ -1,36 +1,32 @@
-""" Contains methods and classes for coordinate transformations.
-
-.. currentmodule:: weldx.transformations
-
-.. rubric:: Classes
-
-.. autosummary::
-  :toctree:
-  :template: class-template.rst
-  :nosignatures:
-
-  CoordinateSystemManager
-  LocalCoordinateSystem
-  WXRotation
-
-.. rubric:: Functions
-
-.. autosummary::
-   :toctree:
-   :nosignatures:
-
-   scale_matrix
-   normalize
-   orientation_point_plane_containing_origin
-   orientation_point_plane
-   is_orthogonal
-   is_orthogonal_matrix
-   point_left_of_line
-   reflection_sign
-   vector_points_to_left_of_vector
-
-"""
+"""Contains methods and classes for coordinate transformations."""
 from .cs_manager import CoordinateSystemManager
 from .local_cs import LocalCoordinateSystem
 from .rotation import WXRotation
 from .util import *
+
+__all__ = [
+    "CoordinateSystemManager",
+    "LocalCoordinateSystem",
+    "WXRotation",
+    "is_orthogonal",
+    "is_orthogonal_matrix",
+    "normalize",
+    "orientation_point_plane",
+    "orientation_point_plane_containing_origin",
+    "point_left_of_line",
+    "reflection_sign",
+    "scale_matrix",
+    "vector_points_to_left_of_vector",
+]
+
+
+def _patch_mod_all():
+    import sys
+
+    mod = sys.modules[__name__]
+    for name in __all__:
+        obj = getattr(mod, name)
+        obj.__module__ = __name__
+
+
+#_patch_mod_all()
