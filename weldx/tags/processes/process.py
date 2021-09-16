e58417e11be1a1c0270bd7a4ccc23fbaf30bcd05
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-09-16 21:08:59 +0200

    match GMAW process tag against pattern

--- weldx/tags/processes/process.py
+++ weldx/tags/processes/process.py
@@ -1,6 +1,7 @@
 """Welding process ASDF classes."""
 
 from weldx.asdf.types import WeldxConverter, format_tag
+from weldx.asdf.util import uri_match
 from weldx.welding.processes import GmawProcess
 
 __all__ = ["GmawProcessConverter"]
@@ -27,6 +28,6 @@ class GmawProcessConverter(WeldxConverter):
     def select_tag(self, obj: GmawProcess, tags, ctx):
         """Select new style tag according to groove name."""
         tag = format_tag(tag_name="process/" + obj.tag, version="0.1.0")
-        if tag not in self.tags:
+        if not uri_match(patterns=self.tags, uri=tag):
             raise ValueError("The generated process tag is not supported")
         return tag
