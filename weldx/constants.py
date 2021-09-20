8c2980a1bdb12c4639859a84946c5a14889e8983
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-09-20 19:59:45 +0200

    apply same hack for U as Q to be included in toctree

--- weldx/constants.py
+++ weldx/constants.py
@@ -50,6 +50,8 @@ lets combine length and time to get a velocity.
 __test__ = {"Q": Q_.__doc__}  # enable doctest checking.
 
 U_ = WELDX_UNIT_REGISTRY.Unit
+U_.__module__ = "pint.unit"  # skipcq: PYL-W0212
+
 
 __all__ = (
     "WELDX_PATH",
