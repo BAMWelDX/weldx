da11f1edf240c3e0c9079671366053da9b237c06
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-09-20 12:30:42 +0200

    use DatetimeIndex in test_meta_attr

--- weldx/tests/asdf_tests/test_asdf_types.py
+++ weldx/tests/asdf_tests/test_asdf_types.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from weldx.asdf.types import META_ATTR, USER_ATTR
@@ -8,8 +9,8 @@ from weldx.measurement import Error
 def test_meta_attr():
     e = Error(3.0)
 
-    ts = pd.Timestamp("2020-01-01")
-    setattr(ts, META_ATTR, {"name": "Timestamp"})
+    ts = pd.DatetimeIndex(["2020", "2021"])
+    setattr(ts, META_ATTR, {"name": "reference years"})
 
     setattr(e, META_ATTR, {"ts": ts})
     setattr(e, USER_ATTR, {"description": "user info"})
@@ -21,6 +22,6 @@ def test_meta_attr():
     e2 = data["Error"]
 
     assert e2 == e
-    assert getattr(e2, META_ATTR) == getattr(e, META_ATTR)
+    assert np.all(getattr(e2, META_ATTR) == getattr(e, META_ATTR))
     assert getattr(e2, USER_ATTR) == getattr(e, USER_ATTR)
     assert getattr(getattr(e2, META_ATTR)["ts"], META_ATTR) == getattr(ts, META_ATTR)
