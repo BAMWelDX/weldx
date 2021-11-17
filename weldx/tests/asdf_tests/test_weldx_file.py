ec8884f006bc823bd17e3dcec31502bfcdaeb4b9
Martin K. Scherer <m.scherer@fu-berlin.de>
2021-11-17 09:28:37 +0100

    merged master

--- weldx/tests/asdf_tests/test_weldx_file.py
+++ weldx/tests/asdf_tests/test_weldx_file.py
@@ -14,10 +14,10 @@ from jsonschema import ValidationError
 
 from weldx import WeldxFile
 from weldx.asdf.cli.welding_schema import single_pass_weld_example
-from weldx.asdf.file import _PROTECTED_KEYS
+from weldx.asdf.file import _PROTECTED_KEYS, DEFAULT_ARRAY_INLINE_THRESHOLD
 from weldx.asdf.util import get_schema_path
 from weldx.types import SupportsFileReadWrite
-from weldx.util import compare_nested
+from weldx.util import WeldxDeprecationWarning, compare_nested
 
 SINGLE_PASS_SCHEMA = "single_pass_weld-0.1.0"
 
@@ -502,24 +502,28 @@ class TestWeldXFile:
             f.update()
             f.close()
 
-        # compare data
-        assert (
-            pathlib.Path("test.asdf").stat().st_size
-            == pathlib.Path("test.wx").stat().st_size
-        )
+        # file sizes should be almost equal (array inlining in wxfile).
+        a = pathlib.Path("test.asdf").stat().st_size
+        b = pathlib.Path("test.wx").stat().st_size
+        assert a >= b
 
-        def _read(fn):
-            with open(fn, "br") as fh:
-                return fh.read()
+        if a == b:
 
-        assert _read("test.asdf") == _read("test.wx")
+            def _read(fn):
+                with open(fn, "br") as fh:
+                    return fh.read()
 
+            assert _read("test.asdf") == _read("test.wx")
+
+    @pytest.mark.filterwarnings("ignore:You tried to manipulate an ASDF internal")
+    @pytest.mark.filterwarnings("ignore:Call to deprecated function data.")
     @pytest.mark.parametrize("protected_key", _PROTECTED_KEYS)
     def test_cannot_update_del_protected_keys(self, protected_key):
         """Ensure we cannot manipulate protected keys."""
         expected_match = "manipulate an ASDF internal structure"
         warning_type = UserWarning
-        with pytest.raises(KeyError):  # try to obtain key from underlying dict.
+        # try to obtain key from underlying dict.
+        with pytest.raises(KeyError), pytest.warns(WeldxDeprecationWarning):
             _ = self.fh.data[protected_key]
 
         with pytest.warns(warning_type, match=expected_match):
@@ -550,3 +554,41 @@ class TestWeldXFile:
 
         for x in iter(self.fh):
             assert x not in _PROTECTED_KEYS
+
+    @staticmethod
+    def test_array_inline_threshold():
+        """Test array inlining threshold."""
+        x = np.arange(7)
+        buff = WeldxFile(
+            tree=dict(x=x), mode="rw", array_inline_threshold=len(x) + 1
+        ).file_handle
+        buff.seek(0)
+        assert str(list(x)).encode("utf-8") in buff.read()
+
+        # now we create an array longer than the default threshold
+        y = np.arange(DEFAULT_ARRAY_INLINE_THRESHOLD + 3)
+        buff2 = WeldxFile(tree=dict(x=y), mode="rw").file_handle
+        buff2.seek(0)
+        data = buff2.read()
+        assert b"BLOCK" in data
+        assert str(list(y)).encode("utf-8") not in data
+
+    @staticmethod
+    def test_array_inline_threshold_sync():
+        x = np.arange(DEFAULT_ARRAY_INLINE_THRESHOLD + 3)
+
+        with WeldxFile(mode="rw") as wx:
+            wx.update(x=x)
+            wx.sync(array_inline_threshold=len(x) + 1)
+            buff3 = wx.file_handle
+            buff3.seek(0)
+            data3 = buff3.read()
+            assert b"BLOCK" not in data3
+
+    @staticmethod
+    def test_array_inline_threshold_write_to():
+        x = np.arange(DEFAULT_ARRAY_INLINE_THRESHOLD + 3)
+        buff = WeldxFile(tree=dict(x=x), mode="rw").write_to(
+            array_inline_threshold=len(x) + 1
+        )
+        assert b"BLOCK" not in buff.read()
