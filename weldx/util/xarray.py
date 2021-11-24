7e9e3493bab3e3aa74593d7e000e2b83b765019f
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2021-11-24 15:25:07 +0100

    revert notes

--- weldx/util/xarray.py
+++ weldx/util/xarray.py
@@ -272,10 +272,6 @@ def xr_interp_like(
         form, meaning: The xarray data will be a quantity and coordinates will have a
         ``Unit`` object in their attributes.
 
-    See Also
-    --------
-    xarray.DataArray.interp_like
-
     """
     da1 = da1.weldx.time_ref_unset()  # catch time formats
     if isinstance(da2, (xr.DataArray, xr.Dataset)):
