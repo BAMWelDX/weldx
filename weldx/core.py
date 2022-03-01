f1ca3d5573b05acceacf5b061e51be2a0fad62bf
vhirtham <volker.hirthammer@bam.de>
2022-03-01 13:53:27 +0100

    run pre-commit

--- weldx/core.py
+++ weldx/core.py
@@ -824,62 +824,62 @@ class GenericSeries:
 
     _allowed_variables: list[str] = []
     """A list of allowed variable names.
-    
+
     The names are only relevant if the created `GenericSeries` is based on expressions.
-    If the expression contains any other variable name that is not part of the list an, 
+    If the expression contains any other variable name that is not part of the list an,
     an exception is raised. It is not required that an expression includes all these
     variables. Additionally, the expression can contain other symbols if they are used
     as parameters.
     """
     _required_variables: list[str] = []
     """A list of required variable names.
-    
+
     The names are only relevant if the created `GenericSeries` is based on expressions.
-    If one or more variables are missing in the expression, an exceptions is raised. 
-    Note, that the required symbols must be variables of the expression. Using one or 
-    more as a parameter will also trigger an exception. 
+    If one or more variables are missing in the expression, an exceptions is raised.
+    Note, that the required symbols must be variables of the expression. Using one or
+    more as a parameter will also trigger an exception.
     """
 
     _evaluation_preprocessor: dict[str, Callable] = {}
     """A dictionary of preprocessor functions for input variables during evaluation.
-    
+
     When evaluating a `GenericSeries`, the passed keyword arguments are checked against
     the dictionaries keys. If a match is found, the corresponding preprocessor function
-    is called with the variables value and returns the updated value. As an example, 
-    this can be used to support multiple time formats. The key might be ``t`` and the 
-    preprocessor function would turn the original time data into an equivalent 
-    `xarray.DataArray`.  
+    is called with the variables value and returns the updated value. As an example,
+    this can be used to support multiple time formats. The key might be ``t`` and the
+    preprocessor function would turn the original time data into an equivalent
+    `xarray.DataArray`.
     """
 
     _required_dimensions: list[str] = []
     """A list of required dimension names.
-    
-    Explicit `GenericSeries` need all of the listed dimensions. Otherwise an exception 
-    is raised. If the series is based on an expression, the dimension can either be 
+
+    Explicit `GenericSeries` need all of the listed dimensions. Otherwise an exception
+    is raised. If the series is based on an expression, the dimension can either be
     represented by a variable or be part of one of the expressions parameters.
     """
 
     _required_dimension_units: dict[str, pint.Unit] = {}
     """A dictionary that maps required units to a dimension.
-    
+
     If a dimension matches one of the keys of this dictionary, it is checked that the
-    units are identical to the requirement. If not, an exception is raised.  
+    units are identical to the requirement. If not, an exception is raised.
     """
     _required_dimension_coordinates: dict[str, list] = {}
     """A dictionary that maps required coordinates to a dimension.
-    
+
     If a dimension matches one of the keys of this dictionary, it is checked if it has
-    the specified coordinates. An example use-case would be a 3d-space where the 
+    the specified coordinates. An example use-case would be a 3d-space where the
     coordinates "x", "y" and "z" are required for a spatial dimension.
     """
 
     _required_unit_dimensionality: pint.Unit = None
     """Required unit dimensionality of the evaluated expression/data.
-    
+
     If the defined unit does not result from the evaluation of the series, an exception
-    is raised. Note that this already checked during construction. If `None`, no 
-    specific unit is required. A unit-less series can be enforced by setting this 
-    setup variable to ``""``. 
+    is raised. Note that this already checked during construction. If `None`, no
+    specific unit is required. A unit-less series can be enforced by setting this
+    setup variable to ``""``.
     """
 
     # do it later
