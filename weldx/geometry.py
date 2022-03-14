7754bd438e372a78beafd49a5f2f41b75681a7af
vhirtham <volker.hirthammer@bam.de>
2022-03-14 13:26:51 +0100

    Add suggestion from PR

--- weldx/geometry.py
+++ weldx/geometry.py
@@ -279,7 +279,10 @@ class DynamicShapeSegment(DynamicBaseSegment):
         raster_width = Q_(raster_width)
         if raster_width <= 0:
             raise ValueError("'raster_width' must be a number greater than 0.")
-        num_pts = np.round((self._length / raster_width).m).astype(int) + 1
+        num_pts = (
+            np.round((self._length / raster_width).to(_DEFAULT_LEN_UNIT).m).astype(int)
+            + 1
+        )
         num_pts = max(num_pts, 2)
 
         vals = np.linspace(0.0, 1.0, num=num_pts, endpoint=True)
