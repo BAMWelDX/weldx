fa46f94791b7492dcf2b2ac4ee10139bd098902e
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2022-03-14 15:35:56 +0100

    lint

--- weldx/geometry.py
+++ weldx/geometry.py
@@ -2935,7 +2935,8 @@ class SpatialData:
 
         """
         mesh = meshio.Mesh(
-            points=self.coordinates.data.reshape(-1,3), cells={"triangle": self.triangles}
+            points=self.coordinates.data.reshape(-1,3),
+            cells={"triangle": self.triangles},
         )
         mesh.write(file_name)
 
