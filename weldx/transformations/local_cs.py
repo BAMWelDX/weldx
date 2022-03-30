Enforce units in LCS and fix some tests

weldx/transformations/local_cs.py
@@ -13,7 +13,7 @@ import xarray as xr
 from scipy.spatial.transform import Rotation as Rot
 
 import weldx.util as ut
-from weldx.constants import _DEFAULT_LEN_UNIT
+from weldx.constants import _DEFAULT_LEN_UNIT, Q_
 from weldx.core import TimeSeries
 from weldx.time import Time, TimeDependent, types_time_like, types_timestamp_like
 from weldx.transformations.types import types_coordinates, types_orientation
@@ -288,7 +288,7 @@ class LocalCoordinateSystem(TimeDependent):
             coordinates = cls._coords_from_discrete_time_series(coordinates)
 
         if coordinates is None:
-            coordinates = np.zeros(3)
+            coordinates = Q_(np.zeros(3), _DEFAULT_LEN_UNIT)
 
         if not isinstance(coordinates, xr.DataArray):
             if not isinstance(coordinates, (np.ndarray, pint.Quantity)):
@@ -296,26 +296,15 @@ class LocalCoordinateSystem(TimeDependent):
 
             coordinates = ut.xr_3d_vector(coordinates, time)
 
-        if isinstance(coordinates.data, pint.Quantity):
-            # The first branch is a workaround until units are mandatory
-            if coordinates.data.u == pint.Unit(""):
-                coordinates.data = coordinates.data.m
-            elif not coordinates.data.is_compatible_with(_DEFAULT_LEN_UNIT):
-                raise pint.DimensionalityError(
-                    coordinates.data.u,
-                    _DEFAULT_LEN_UNIT,
-                    extra_msg="\nThe coordinates require units representing a length.",
-                )
-
-        if not isinstance(coordinates.data, pint.Quantity) and not (
-            coordinates.shape == (3,) and np.allclose(coordinates.data, np.zeros(3))
+        c_data = coordinates.data
+        if not isinstance(c_data, pint.Quantity) or not c_data.is_compatible_with(
+            _DEFAULT_LEN_UNIT
         ):
-
-            warnings.warn(
-                "Coordinates without units are deprecated and won't be supported in "
-                "the future",
-                DeprecationWarning,
-                stacklevel=2,
+            unit = c_data.u if isinstance(c_data, pint.Quantity) else None
+            raise pint.DimensionalityError(
+                unit,
+                _DEFAULT_LEN_UNIT,
+                extra_msg="\nThe coordinates require units representing a length.",
             )
 
         # make sure we have correct "time" format