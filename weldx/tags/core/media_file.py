27d61dd83518dd341ff255f63e56f1be19299767
Martin K. Scherer <m.scherer@fu-berlin.de>
2022-04-05 14:08:52 +0200

    added n_frames to schema

--- weldx/tags/core/media_file.py
+++ weldx/tags/core/media_file.py
@@ -13,7 +13,13 @@ class MediaFileConverter(WeldxConverter):
 
     def to_yaml_tree(self, obj: MediaFile, tag: str, ctx) -> dict:
         """Convert to python dict."""
-        tree = dict(file=obj.file(), reference_time=obj.reference_time)
+        tree = dict(
+            file=obj.file(),
+            reference_time=obj.reference_time,
+            fps=obj.fps,
+            n_frames=len(obj),
+            resolution=obj.resolution,
+        )
         return tree
 
     def from_yaml_tree(self, node: dict, tag: str, ctx):
@@ -21,5 +27,7 @@ class MediaFileConverter(WeldxConverter):
         from weldx.tags.core.file import ExternalFile
 
         file: ExternalFile = node["file"]
-        result = MediaFile(file.path)
+        fps = node["fps"]
+        reference_time = node["reference_time"]
+        result = MediaFile(file.path, fps=fps, reference_time=reference_time)
         return result
