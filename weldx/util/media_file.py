4e119cd80b3c9b4f50b51da6589fc197263f3f4f
Martin K. Scherer <m.scherer@fu-berlin.de>
2022-04-29 10:47:15 +0200

    type safety, iterators guarding

--- weldx/util/media_file.py
+++ weldx/util/media_file.py
@@ -1,5 +1,4 @@
 """Media file."""
-import typing
 from pathlib import Path
 from typing import Optional, Sequence, Union, get_args
 
@@ -12,14 +11,17 @@ from weldx import Q_
 from weldx.tags.core.file import ExternalFile
 from weldx.types import types_path_like
 
+__all__ = ["types_media_input", "MediaFile"]
+
 types_media_input = Union[
     types_path_like,
     Sequence[Sequence[int]],
+    # TODO: reconsider this, we want a list of shape x,y,(2 or 3)
 ]
-
 
 # _pts_to_frame, _get_frame_rate, _get_frame_count taken from
 # https://github.com/PyAV-Org/PyAV/blob/v9.1.1/scratchpad/frame_seek_example.py
+AV_TIME_BASE = 1000000
 
 
 def _pts_to_frame(pts, time_base, frame_rate, start_time):
@@ -36,8 +38,6 @@ def _get_frame_rate(stream):
 
 
 def _get_frame_count(f, stream):
-    AV_TIME_BASE = 1000000
-
     if stream.frames:
         return stream.frames
     if stream.duration:
@@ -76,8 +76,8 @@ class MediaFile:
     def __init__(
         self,
         path_or_array: types_media_input,
-        reference_time: pd.Timestamp = None,
-        fps: float = None,
+        reference_time: Optional[pd.Timestamp] = None,
+        fps: Optional[float] = None,
     ):
         if isinstance(path_or_array, get_args(types_path_like)):
             from dask_image.imread import imread
@@ -85,11 +85,17 @@ class MediaFile:
             path = Path(path_or_array)  # type: ignore[arg-type]
 
             self._handle = imread(path_or_array)
-            self._metadata = self._get_video_properties(str(path))
+            self._metadata = self._get_video_metadata(str(path))
             self._wrap_data_array(path_or_array)
 
             if reference_time is None:
                 self._reference_time = pd.Timestamp(path.stat().st_mtime_ns)
+            elif isinstance(reference_time, pd.Timestamp):
+                self._reference_time = reference_time
+            else:
+                raise ValueError(
+                    f"unsupported type for reference_time {type(reference_time)}"
+                )
             self._from_file = True
         elif isinstance(path_or_array, list):
             self._handle = path_or_array
@@ -105,8 +111,11 @@ class MediaFile:
                 resolution=(image.width, image.height),
                 nframes=len(path_or_array),
             )
-            if reference_time is None:
-                self._reference_time = pd.Timestamp(-1)
+            if not isinstance(reference_time, (None, pd.Timestamp)):
+                raise ValueError(
+                    f"unsupported type for reference_time {type(reference_time)}"
+                )
+            self._reference_time = reference_time
             self._from_file = False
             self._wrap_data_array("from_buffer")
         else:
@@ -124,14 +133,14 @@ class MediaFile:
         self._array.frames.attrs["units"] = "s"
 
     @staticmethod
-    def _get_video_properties(fn: str) -> dict:
+    def _get_video_metadata(fn: str) -> dict:
         import av
 
         v = av.open(fn)
-        frame = next(v.decode())
+        frame = next(v.decode(), None)
         resolution = frame.width, frame.height
 
-        stream = next(s for s in v.streams if s.type == "video")
+        stream = next((s for s in v.streams if s.type == "video"), None)
 
         metadata = dict(
             fps=_get_frame_rate(stream),
@@ -145,8 +154,8 @@ class MediaFile:
         return metadata
 
     @property
-    def reference_time(self) -> pd.Timestamp:
-        """Time of recording this media."""
+    def reference_time(self) -> Optional[pd.Timestamp]:
+        """Time of recording of this media (if known)."""
         # TODO: EXIF tag, plain m-time?
         return self._reference_time
 
@@ -158,7 +167,7 @@ class MediaFile:
         return AttrDict(self._metadata)
 
     @property
-    def resolution(self) -> typing.Tuple[int, int]:
+    def resolution(self) -> tuple[int, int]:
         """Resolution in pixels (widths, height)."""
         return self._metadata["resolution"]
 
@@ -177,12 +186,12 @@ class MediaFile:
         # TODO: this will rehash every time we serialize
         # even if the underlying path is unchanged.
         if not self._from_file:
-            one_buff = bytes(np.array(self._path_or_array))
+            # TODO: why wrap in an array again?
+            buffer = bytes(np.array(self._path_or_array))
             return ExternalFile(
-                buffer=one_buff, filename="<in-memory-source>", asdf_save_content=True
+                buffer=buffer, filename="<in-memory-source>", asdf_save_content=True
             )
-        else:
-            return ExternalFile(self._path_or_array)  # type: ignore[arg-type]
+        return ExternalFile(self._path_or_array)  # type: ignore[arg-type]
 
     @property
     def data(self) -> xr.DataArray:
