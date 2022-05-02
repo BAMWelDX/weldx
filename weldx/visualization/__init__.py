cf58557d998c9d49100386e4090ab43aec0be030
Martin K. Scherer <m.scherer@fu-berlin.de>
2022-05-02 16:40:43 +0200

    import guard

--- weldx/visualization/__init__.py
+++ weldx/visualization/__init__.py
@@ -2,29 +2,33 @@
 
 The implementation details are now served by the "weldx-widgets" package (since 0.6.1).
 """
+try:
+    from weldx_widgets.visualization.csm_k3d import (
+        CoordinateSystemManagerVisualizerK3D,
+        SpatialDataVisualizer,
+    )
+    from weldx_widgets.visualization.csm_mpl import (
+        axes_equal,
+        draw_coordinate_system_matplotlib,
+        new_3d_figure_and_axes,
+        plot_coordinate_system_manager_matplotlib,
+        plot_coordinate_systems,
+        plot_local_coordinate_system_matplotlib,
+        plot_spatial_data_matplotlib,
+    )
 
-from weldx_widgets.visualization.csm_k3d import (
-    CoordinateSystemManagerVisualizerK3D,
-    SpatialDataVisualizer,
-)
-from weldx_widgets.visualization.csm_mpl import (
-    axes_equal,
-    draw_coordinate_system_matplotlib,
-    new_3d_figure_and_axes,
-    plot_coordinate_system_manager_matplotlib,
-    plot_coordinate_systems,
-    plot_local_coordinate_system_matplotlib,
-    plot_spatial_data_matplotlib,
-)
-
-__all__ = (
-    "CoordinateSystemManagerVisualizerK3D",
-    "SpatialDataVisualizer",
-    "axes_equal",
-    "draw_coordinate_system_matplotlib",
-    "new_3d_figure_and_axes",
-    "plot_coordinate_system_manager_matplotlib",
-    "plot_coordinate_systems",
-    "plot_local_coordinate_system_matplotlib",
-    "plot_spatial_data_matplotlib",
-)
+    __all__ = (
+        "CoordinateSystemManagerVisualizerK3D",
+        "SpatialDataVisualizer",
+        "axes_equal",
+        "draw_coordinate_system_matplotlib",
+        "new_3d_figure_and_axes",
+        "plot_coordinate_system_manager_matplotlib",
+        "plot_coordinate_systems",
+        "plot_local_coordinate_system_matplotlib",
+        "plot_spatial_data_matplotlib",
+    )
+except ModuleNotFoundError:
+    raise RuntimeError(
+        "If you demand visualization in weldx, " "please install `weldx_widgets`"
+    )
