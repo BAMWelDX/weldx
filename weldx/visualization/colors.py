a8cfd9e188320c28dab5b5a30755cc48d2c5ca9d
Cagtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2022-05-13 20:26:14 +0200

    remove old visualization modules (#755)
    * remove old visualization modules
    
    * test with widgets master
    
    * fetch tags
    
    * set back to use widgets from conda

--- weldx/visualization/colors.py
+++
@@ -1,3 +0,0 @@
-"""Colors."""
-# backward compat. Remove for wx-widgets 0.2.2
-from weldx_widgets.visualization.colors import *  # noqa: F401,F403
--- weldx/visualization/types.py
+++
@@ -1,3 +0,0 @@
-"""Types."""
-# backward compat. Remove for wx-widgets 0.2.2
-from weldx_widgets.visualization.types import *  # noqa: F401,F403
