39b662d1a5d9b61c8cb24535e82de03f76a6aace
vhirtham <20574817+vhirtham@users.noreply.github.com>
2022-07-20 17:50:05 +0200

    Add test for installable QS

---
+++ weldx/tests/asdf_tests/installable_quality_standard_check.py
@@ -0,0 +1,20 @@
+import pytest
+from asdf import ValidationError
+
+from weldx import WeldxFile
+from weldx.config import enable_quality_standard
+from weldx.measurement import MeasurementEquipment
+
+
+def test_installable_quality_standard():
+    enable_quality_standard("quality_standard_demo")
+
+    eq = MeasurementEquipment("Equipment")
+
+    tree = dict(equip=eq)
+
+    with pytest.raises(ValidationError):
+        WeldxFile(tree=tree, mode="rw")
+
+    eq.wx_metadata = {"serial_number": 1234}
+    WeldxFile(tree=tree, mode="rw")
