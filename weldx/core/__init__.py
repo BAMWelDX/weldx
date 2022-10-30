4e32e83abf1869291fd13ce6dc2944c82c9f12d1
Martin K. Scherer <m.scherer@fu-berlin.de>
2022-10-30 13:53:09 +0100

    patch mod of "core" pkg

--- weldx/core/__init__.py
+++ weldx/core/__init__.py
@@ -6,3 +6,9 @@ from weldx.core.spatial_series import SpatialSeries
 from weldx.core.time_series import TimeSeries
 
 __all__ = ["MathematicalExpression", "GenericSeries", "SpatialSeries", "TimeSeries"]
+
+
+from ..util import _patch_mod_all  # noqa
+
+_patch_mod_all("weldx.core")
+del _patch_mod_all
