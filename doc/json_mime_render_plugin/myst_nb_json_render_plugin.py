b661910dfd56092259d9ba15ed38c5f53e8f0fba
Martin K. Scherer <m.scherer@fu-berlin.de>
2023-01-26 14:09:56 +0100

    clean up

--- doc/json_mime_render_plugin/myst_nb_json_render_plugin.py
+++ doc/json_mime_render_plugin/myst_nb_json_render_plugin.py
@@ -1,13 +1,7 @@
 import itertools
 
 from docutils import nodes
-from IPython.core.display import JSON
 from myst_nb.core.render import MimeData, NbElementRenderer
-from sphinx.util.logging import getLogger
-
-log = getLogger("sphinx")
-
-JSON
 
 
 class MimeRenderPlugin:
@@ -17,8 +11,6 @@ class MimeRenderPlugin:
 
     @staticmethod
     def handle_mime(renderer: NbElementRenderer, data: MimeData, inline: bool):
-        # TODO: what about inline?
-        # if not inline and data.mime_type == "application/json":
         if data.mime_type == "application/json":
             json_id = next(MimeRenderPlugin.json_ids)
             html = (
@@ -26,7 +18,6 @@ class MimeRenderPlugin:
                 f"<script>document.getElementById('json-{json_id}').textContent = "
                 f"JSON.stringify({data.string}, undefined, 2);</script>"
             )
-            log.info("html: %s", html)
             return [
                 nodes.raw(text=html, format="html", classes=["output", "text_html"]),
             ]
