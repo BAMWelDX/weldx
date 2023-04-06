9cb7f08daf5614bc183d094ce415d3aa8ce8be5a
Çağtay Fabry <43667554+CagtayFabry@users.noreply.github.com>
2023-04-06 17:23:37 +0200

    v0.6.5 (#866)
    * drop Python 3.8
    
    * update CHANGELOG.md
    
    * update pull_request_template.md
    
    * update CITATION.cff

--- devtools/scripts/pyupgrad_run.py
+++ devtools/scripts/pyupgrad_run.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     files = Path("./../../weldx/").rglob("*.py")
     for p in files:
         subprocess.run(  # skipcq: BAN-B607
-            ["pyupgrade", str(p.resolve()), "--py38-plus", "--keep-runtime-typing"],
+            ["pyupgrade", str(p.resolve()), "--py39-plus", "--keep-runtime-typing"],
             check=True,
         )
