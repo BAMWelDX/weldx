857dd14b171d898c18548397eab5d5d8dc05e952
Çağtay Fabry <cagtay.fabry@bam.de>
2026-08-21 13:56:43 +0200

    py314, add min-deps env, migrate to pytest-skip-slow plugin (#1039)
    * migrate slow test skipping to pytest-skip-slow plugin
    
    - add pytest-skip-slow to test optional-dependencies and pixi test feature
    - remove manual --runslow option and pytest_collection_modifyitems hook from conftest.py
    - update pytest command in GitHub Actions workflow to use --slow
    - update CHANGELOG.md for 0.7.4
    
    * update changelog
    
    * add py314
    
    * update lockfile
    
    * add codecov oidc auth
    
    * update build workflow for publishing
    
    * rename to build.yml

--- conftest.py
+++ conftest.py
@@ -17,9 +17,6 @@ def mock_rw_buffer_weldxfile(request, monkeypatch):
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-    parser.addoption(
         "--weldx-file-rw-buffer",
         action="store_true",
         default=False,
@@ -31,13 +28,3 @@ def pytest_addoption(parser):
         default=False,
         help="invoke display header to provoke side effects",
     )
-
-
-def pytest_collection_modifyitems(config, items):
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
